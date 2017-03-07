961694eb978349c225eab392ae2dd3ce41baea03
Bruno Castro <bruno.castro@codeminer42.com>
2017-03-07 11:46:44 -0300

    Add integrations to Project endpoint When queried a particular project, it now returns the integrations corresponding to the project.

--- app/api/entities/project.rb
+++ app/api/entities/project.rb
@@ -8,7 +8,8 @@ class Entities::Project < Entities::BaseEntity
   expose :default_velocity
   expose :velocity, if: { type: :full }
   expose :volatility, if: { type: :full }
-  expose :teams
+  expose :teams, using: API::Entities::Team
+  expose :integrations, using: API::Entities::Integration, if: { type: :full } 
 
   with_options(format_with: :iso_timestamp) do
     expose :created_at, if: lambda { |i, o| i.created_at.present? }
