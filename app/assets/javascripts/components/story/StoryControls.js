65f0e0b82b9575a24db1fa8f8e181b644e392008
Edgar Yonemura <edgar.yonemura@gmail.com>
2017-04-03 15:13:24 -0300

    Add react component to story controls

---
+++ app/assets/javascripts/components/story/StoryControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StoryControls = (props) => {
+  return(
+    <div className="form-group story-controls">
+      <input className="submit" onClick={props.onClickSave} type="button" value="Save" />
+      <input className="destroy" onClick={props.onClickDelete} type="button" value="Delete" />
+      <input className="cancel" onClick={props.onClickCancel} type="button" value="Cancel" />
+    </div>
+  );
+};
+
+export default StoryControls;
