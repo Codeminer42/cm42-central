d7ef5a2c55db2dffd1731dd8e3023332a80beee4
Leonardo Freitas <leonardo.freitas@codeminer42.com>
2017-07-13 12:06:05 -0300

    Fixing I18N dependency and rspec webpacker

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.configure do |config|
   config.before(:suite) do
     %x[bundle exec rake assets:precompile]
-    %x[bundle exec rake webpack:compile]
+    %x[bundle exec rake webpacker:compile]
   end
 
   config.include Warden::Test::Helpers
