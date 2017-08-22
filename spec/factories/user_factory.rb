823c838b879d53a302f96eebfbd4edfd6bd8d0f2
Diêggo Faustino <dieggo.faustino@codeminer42.com>
2017-08-22 10:46:50 -0300

    [Rubocop] Add space inside curly brackets

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
   factory :user do |u|
-    u.sequence(:name) {|n| "User #{n}"}
-    u.sequence(:username) {|n| "username #{n}"}
-    u.sequence(:initials) {|n| "U#{n}"}
-    u.sequence(:email) {|n| "user#{n}@example.com"}
+    u.sequence(:name) { |n| "User #{n}" }
+    u.sequence(:username) { |n| "username #{n}" }
+    u.sequence(:initials) { |n| "U#{n}" }
+    u.sequence(:email) { |n| "user#{n}@example.com" }
     u.password 'password'
     u.password_confirmation 'password'
     u.locale 'en'
     u.time_zone 'Brasilia'
     u.finished_tour true
-    u.after(:build) {|user| user.confirm }
+    u.after(:build) { |user| user.confirm }
 
     trait :with_team do
       after(:build) { |object| object.enrollments.create(team: create(:team), is_admin: false ) }
@@ -21,9 +21,9 @@ FactoryGirl.define do
   end
 
   factory :unconfirmed_user, class: User do |u|
-    u.sequence(:name) {|n| "Unconfirmed User #{n}"}
-    u.sequence(:username) {|n| "testuser#{n}"}
-    u.sequence(:initials) {|n| "U#{n}"}
-    u.sequence(:email) {|n| "unconfirmed_user#{n}@example.com"}
+    u.sequence(:name) { |n| "Unconfirmed User #{n}" }
+    u.sequence(:username) { |n| "testuser#{n}" }
+    u.sequence(:initials) { |n| "U#{n}" }
+    u.sequence(:email) { |n| "unconfirmed_user#{n}@example.com" }
   end
 end
