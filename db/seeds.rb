1d2cef3d38b5f707ffff8fff023650f3afe86ede
Diêggo Faustino <dieggo.faustino@codeminer42.com>
2017-08-23 10:52:59 -0300

    Add db/seeds.rb to rubocop analysis

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ ActiveRecord::Base.transaction do
     initials: 'FB',
     username: 'foobar',
     email: 'foo@bar.com',
-    password: 'asdfasdf',
+    password: 'asdfasdf'
   )
 
   user.confirm!
@@ -12,58 +12,59 @@ ActiveRecord::Base.transaction do
   project = Project.create!(
     name: 'Test Project',
     users: [user],
-    start_date: Time.now,
+    start_date: Time.now
   )
 
   project.stories.create!(
-    title: "A user should be able to create features",
+    title: 'A user should be able to create features',
     story_type: 'feature',
     requested_by: user,
-    labels: 'features',
+    labels: 'features'
   )
 
   project.stories.create!(
-    title: "A user should be able to create bugs",
+    title: 'A user should be able to create bugs',
     story_type: 'bug',
     requested_by: user,
-    labels: 'bugs',
+    labels: 'bugs'
   )
 
   project.stories.create!(
-    title: "A user should be able to create chores",
+    title: 'A user should be able to create chores',
     story_type: 'chore',
     requested_by: user,
-    labels: 'chores',
+    labels: 'chores'
   )
 
   project.stories.create!(
-    title: "A user should be able to create releases",
+    title: 'A user should be able to create releases',
     story_type: 'release',
     requested_by: user,
-    labels: 'releases',
+    labels: 'releases'
   )
 
   project.stories.create!(
-    title: "A user should be able to estimate features",
+    title: 'A user should be able to estimate features',
     story_type: 'feature',
     requested_by: user,
     estimate: 1,
-    labels: 'estimates,features',
+    labels: 'estimates,features'
   )
-
 
   2.times do |n|
     project.stories.first.notes.create!(
       note: "This is comment number #{n + 1}",
-      user: user,
+      user: user
     )
   end
-
 
   team = Team.create! name: 'Default'
   team.enrollments.create! user: user, is_admin: true
   team.projects << project
 
-
-  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+  AdminUser.create!(
+    email: 'admin@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+  )
 end
