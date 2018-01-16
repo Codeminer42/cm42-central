0db0ce34a6a12f07e6ffab54458de3b5311ccc07
Talysson <talyssonoc@gmail.com>
2018-01-16 10:37:55 -0200

    Refactor StoryStateButton components

--- app/assets/javascripts/components/story/StoryStateButtons.js
+++ app/assets/javascripts/components/story/StoryStateButtons.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const renderInputButtons = (events) =>
-  events.map((event) =>
-    <input
-      type="button"
-      key={event}
-      className={`transition ${event}`}
-      value={I18n.t('story.events.' + event)}
-    />
-)
+const renderInputButton = (event) => (
+  <input
+    type="button"
+    key={event}
+    className={`transition ${event}`}
+    value={I18n.t('story.events.' + event)}
+  />
+);
 
-const StateButtons = (props) =>
+const StateButtons = ({ events, isSearchResult }) => (
   <div className="state-actions">
-    { renderInputButtons(props.events) }
-    { props.isSearchResult &&
+    { events.map(renderInputButton) }
+    { isSearchResult &&
       <button id="locate" type="button" className="btn btn-default locate-btn locate">
         <i className="mi md-gps-fixed md-14">gps_fixed</i>
       </button>
     }
   </div>
+);
 
-export default StateButtons
+export default StateButtons;
