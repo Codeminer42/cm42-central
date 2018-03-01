ea73212be15c92ab0223cb8c262618fd35a1176d
Hayssa Costa <hayssa.hscosta@gmail.com>
2018-03-01 10:29:54 -0300

    Correcting function to pass tests

--- app/assets/javascripts/collections/story_collection.js
+++ app/assets/javascripts/collections/story_collection.js
@@ -29,22 +29,20 @@ module.exports = Backbone.Collection.extend({
 
     var thisStory = this.get(thisId);
     var previousStory = this.get(previousStoryId);
-    var thisStoryPosition = thisStory.position();
+    var thisStoryPosition = this.round(thisStory.position(), MAX_DECIMAL_PRECISION);
 
     if (typeof previousStory !== 'undefined') {
       var previousStoryPosition = previousStory.position();
       if (thisStoryPosition <= previousStoryPosition) {
         const difference = Math.abs(thisStoryPosition - previousStoryPosition) + correctionFactor;
-        
         thisStoryPosition = thisStoryPosition + difference;
         thisStory.set({ position: this.round(thisStoryPosition, MAX_DECIMAL_PRECISION) });
-        
-        var nextStory = this.nextOnColumn(thisStory);
-        
-        if (typeof nextStory !== 'undefined') {
-          return this.roundPosition(nextStory.id, thisId);
-        }
       }
+    }
+
+    var nextStory = this.nextOnColumn(thisStory);
+    if (typeof nextStory !== 'undefined' && nextStory.position() <= thisStoryPosition) {
+      this.roundPosition(nextStory.id, thisId);
     }
   },
 
