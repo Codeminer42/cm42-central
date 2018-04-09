e1ad4e4df6b67940d154d2f539907961d3df24d7
br-silva <brunorafael6656@gmail.com>
2018-04-09 10:24:41 -0300

    Add refatoring for new models tests
    Add little refatoring to improve readability

--- spec/models/iterations/past_iteration_spec.rb
+++ spec/models/iterations/past_iteration_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 module Iterations
   describe PastIteration do
     let(:project) { create(:project, :with_past_date) }
@@ -7,18 +8,22 @@ module Iterations
         title: 'Done story',
         state: 'accepted',
         estimate: 8,
-        accepted_at: project.created_at,
+        accepted_at: project.created_at
       }
     end
+
+    let(:past_iteration_params) do
+      { start_date: project.created_at, end_date: project.created_at + 7, project: project }
+    end
+
+    subject { described_class.new(past_iteration_params) }
 
     before do
       3.times { project.stories.create!(done_story_params) }
     end
-    
-    subject { PastIteration.new(start_date: project.created_at, end_date: project.created_at + 7, project: project) }
 
     describe '#points' do
-      it 'sums the estimate of the stories in this iteration' do
+      it 'sums the story estimates in this iteration' do
         expect(subject.points).to eq(done_story_params[:estimate] * 3)
       end
     end
--- spec/models/iterations/project_iterations_spec.rb
+++ spec/models/iterations/project_iterations_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
+
 module Iterations
   describe ProjectIterations do
-
     describe '#current_iteration_start' do
-      let(:project) { create(:project) } 
-      subject { ProjectIterations.new(project: project) }
+      let(:project) { create(:project) }
+      subject       { described_class.new(project: project) }
 
-      it 'returns the start date of the current iteration' do
+      it 'returns the current iteration start date' do
         expect(subject.current_iteration_start).to eq(DateTime.current.to_date)
       end
     end
 
     describe '#past_iterations' do
-      let(:project_with_past_date) { create(:project, :with_past_date) }
-      subject { ProjectIterations.new(project: project_with_past_date) }
+      let(:past_date_project) { create(:project, :with_past_date) }
+      subject                 { described_class.new(project: past_date_project) }
 
       it 'returns a past iteration' do
         expect(subject.past_iterations).to all(be_a(PastIteration))
