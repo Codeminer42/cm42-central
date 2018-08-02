336f7e0a0057caf6c3960ff4f9fc8bd5becd0d84
Talysson <talyssonoc@gmail.com>
2018-08-02 10:56:39 -0300

    Send active stories and past iterations separately in v2

--- app/models/project_board.rb
+++ app/models/project_board.rb
@@ -4,6 +4,7 @@ class ProjectBoard
   attr_accessor :project
   attr_accessor :users
   attr_accessor :stories
+  attr_accessor :past_iterations
   attr_accessor :current_user
   attr_accessor :current_flow
   attr_accessor :default_flow
