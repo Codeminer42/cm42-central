b2fee8d5c07860c958cae9307d092b1ec67d60cf
Talysson <talyssonoc@gmail.com>
2018-09-10 15:50:07 -0300

    Use env vars only in test and dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,13 @@ Rails.application.configure do
     Bullet.rails_logger = true
     Bullet.add_footer = true
   end
+
+  require 'pusher'
+
+  Pusher.app_id = ENV['PUSHER_APP_ID']
+  Pusher.key = ENV['PUSHER_APP_KEY']
+  Pusher.secret = ENV['PUSHER_APP_SECRET']
+  Pusher.cluster = ENV['PUSHER_APP_CLUSTER']
+  Pusher.logger = Rails.logger
+  Pusher.encrypted = true
 end
--- config/initializers/pusher.rb
+++
@@ -1,8 +0,0 @@
-require 'pusher'
-
-Pusher.app_id = ENV['PUSHER_APP_ID']
-Pusher.key = ENV['PUSHER_APP_KEY']
-Pusher.secret = ENV['PUSHER_APP_SECRET']
-Pusher.cluster = ENV['PUSHER_APP_CLUSTER']
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
