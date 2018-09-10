f5e87c765bb7c0f07e06e1a0b7c849829f5602f5
Talysson <talyssonoc@gmail.com>
2018-09-10 17:08:57 -0300

    Extract credentials from WS URL

--- app/assets/javascripts/pusherSockets.js
+++ app/assets/javascripts/pusherSockets.js
@@ -1,7 +1,14 @@
 import Pusher from 'pusher-js';
 
-const pusherApiKey = process.env.PUSHER_APP_KEY;
-const pusherCluster = process.env.PUSHER_APP_CLUSTER;
+const pusherArgsRegex = /\/\/ws-(.*)\.pusher.*\/(.*)$/;
+
+const pusherUrl = process.env.PUSHER_SOCKET_URL;
+
+const [
+  _, // whole match
+  pusherCluster,
+  pusherApiKey
+] = pusherUrl.match(pusherArgsRegex);
 
 export const subscribeToProjectChanges = (project, callback) => {
   const socket = getProjectSocket();
