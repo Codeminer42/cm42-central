30a2e1e89e2313f0d81bb56663b9cbac08a1f983
Talysson <talyssonoc@gmail.com>
2018-09-11 11:31:21 -0300

    Travis and Capybara setup

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,14 +3,13 @@ require 'capybara-screenshot/rspec'
 require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[ no-sandbox headless disable-popup-blocking disable-gpu window-size=1280,1024]
-    }
-  )
+  args = %w[no-sandbox headless disable-popup-blocking disable-gpu window-size=1280,1024]
+  options = Selenium::WebDriver::Chrome::Options.new(args: args)
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
+
+Capybara.javascript_driver = :chrome
 
 Capybara::Screenshot.register_driver :chrome do |driver, path|
   driver.save_screenshot(path)
