0dd2ac6def8381b14c3f5031e19a4f99b2f227d5
Talysson <talyssonoc@gmail.com>
2018-09-12 11:33:54 -0300

    Tweak capybara options

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,17 +2,36 @@ require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 require 'selenium-webdriver'
 
-Capybara.register_driver :chrome do |app|
-  args = %w[no-sandbox headless disable-popup-blocking disable-gpu window-size=1280,1024]
-  options = Selenium::WebDriver::Chrome::Options.new(args: args)
+# Capybara.register_driver :chrome do |app|
+#   args = %w[no-sandbox headless disable-popup-blocking disable-gpu window-size=1280,1024]
+#   options = Selenium::WebDriver::Chrome::Options.new(args: args)
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
+
+Capybara.register_driver :chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[ no-sandbox headless disable-popup-blocking disable-gpu disable-infobars window-size=1280,1024]
+    }
+  )
+
+  client = Selenium::WebDriver::Remote::Http::Default.new(read_timeout: 120)
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities,
+    http_client: client
+  )
 end
+
 
 Capybara::Screenshot.register_driver :chrome do |driver, path|
   driver.save_screenshot(path)
 end
 
+Capybara.default_driver = :chrome
 Capybara.javascript_driver = :chrome
 
 Capybara.default_max_wait_time = 100
