cfdd6237be0e809f4fbd2bc0af69c16c542b56c8
kostadriano <adrianokosta001@gmail.com>
2018-10-10 15:27:48 -0300

    refactor ProjectIterations

--- app/models/iterations/project_iterations.rb
+++ app/models/iterations/project_iterations.rb
@@ -7,13 +7,13 @@ module Iterations
     end
 
     def current_iteration_start
-      first_sprint_start_date + (length * iteration_length_in_days)
+      first_iteration_start_date + (number_of_iterations * iteration_length_in_days)
     end
 
     def past_iterations
-      (0...length).map do |iteration_number|
-        start_at = interation_start_date(iteration_number)
-        end_at = interation_end_date(start_at)
+      (0...number_of_iterations).map do |iteration_number|
+        start_at = iteration_start_date(iteration_number)
+        end_at = iteration_end_date(start_at)
 
         PastIteration.new(
           start_date: start_at,
@@ -45,33 +45,33 @@ module Iterations
       end
     end
 
-    def length
-      (days_since_project_start / iteration_length_in_days).floor
+    def missing_days_from_first_sprint
+      project.start_date.wday - project.iteration_start_day
+    end
+
+    def first_iteration_start_date
+      project.start_date - missing_days_from_first_sprint
+    end
+
+    def number_of_iterations
+      (days_since_first_iteration_start / iteration_length_in_days).floor
     end
 
     def iteration_length_in_days
       project.iteration_length * DAYS_IN_A_WEEK
     end
 
-    def project_start_from_first_sprint
-      project.start_date.wday - project.iteration_start_day
+    def iteration_start_date(iteration_number)
+      iteration_days = iteration_number * iteration_length_in_days
+      first_iteration_start_date + iteration_days
     end
 
-    def first_sprint_start_date
-      project.start_date - project_start_from_first_sprint
+    def iteration_end_date(start_date)
+      start_date + (iteration_length_in_days - 1)
     end
 
-    def interation_start_date(iteration_number)
-      iteration_days = (iteration_number * iteration_length_in_days)
-      (first_sprint_start_date + iteration_days)
-    end
-
-    def interation_end_date(start_date)
-      (start_date + (iteration_length_in_days - 1))
-    end
-
-    def days_since_project_start
-      Date.current - first_sprint_start_date
+    def days_since_first_iteration_start
+      Date.current - first_iteration_start_date
     end
   end
 end
