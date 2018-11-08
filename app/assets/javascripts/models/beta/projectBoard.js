beb8914f16d835022d39e0af56610672f9add71f
Leonardo Negreiros <leonardo.negreiros@codeminer42.com>
2018-11-08 17:14:22 -0300

    Change projectBoard object
    Now it sends the notes in a format easier to work on the views

--- app/assets/javascripts/models/beta/projectBoard.js
+++ app/assets/javascripts/models/beta/projectBoard.js
@@ -4,5 +4,14 @@ import camelCase from 'camelcase-object-deep';
 export function get(projectId) {
   return axios
     .get(`/beta/project_boards/${projectId}`)
-    .then(({ data }) => camelCase(data, {deep: true}));
+    .then(({ data }) => camelCase(data, {deep: true}))
+    .then(( projectBoard ) => ({
+        ...projectBoard,
+        stories: project.stories.map((story) => ({
+          ...story,
+          notes: story.notes.map((note) => ({
+            note: note.note                
+          }))
+        }))
+      }));
 }
