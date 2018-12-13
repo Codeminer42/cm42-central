748df8171e39fbbcbbf3681f3ec343fb85341475
Adriano Costa <adrianokosta001@gmail.com>
2018-12-13 10:56:08 -0200

    Remove camelcase-object-deep dependency

--- app/assets/javascripts/models/beta/projectBoard.js
+++ app/assets/javascripts/models/beta/projectBoard.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import camelCase from 'camelcase-object-deep';
+import changeCase from'change-object-case';
+
 
 export function get(projectId) {
   return axios
     .get(`/beta/project_boards/${projectId}`)
-    .then(({ data }) => camelCase(data, {deep: true}))
+    .then(({ data }) => changeCase.camelKeys(data, {recursive: true, arrayRecursive: true}))
     // This was necessary because the backend wasn't sending the notes on the stories in the right format,
     // This change act as a palliative measure until the backend is fixed
     .then(( projectBoard ) => ({
