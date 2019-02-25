269e30043f92f37c610ecbc4f9cccaba87270cc8
Talysson de Oliveira Cassiano <talyssonoc@gmail.com>
2019-02-25 11:35:58 -0300

    Move error handling to the correct place (#520)

--- app/assets/javascripts/actions/note.js
+++ app/assets/javascripts/actions/note.js
@@ -15,9 +15,11 @@ export const createNoteSuccess = (storyId, note) => ({
 export const deleteNote = (projectId, storyId, noteId) =>
   (dispatch, getState, { Note }) =>
     Note.destroy(projectId, storyId, noteId)
-      .then(() => dispatch(deleteNoteSuccess(storyId, noteId)));
+      .then(() => dispatch(deleteNoteSuccess(storyId, noteId)))
+      .catch((error) => console.error(error));
 
 export const createNote = (projectId, storyId, note) =>
   (dispatch, getState, { Note }) =>
     Note.post(projectId, storyId, note)
-      .then((note) => dispatch(createNoteSuccess(storyId, note)));
+      .then((note) => dispatch(createNoteSuccess(storyId, note)))
+      .catch((error) => console.error(error));
