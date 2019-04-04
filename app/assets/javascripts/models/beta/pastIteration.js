31aa6152e26990012a1382f4eae28c483428ccb1
jprask <jpraskys@gmail.com>
2019-04-04 17:50:41 -0300

    Create past iteration model

---
+++ app/assets/javascripts/models/beta/pastIteration.js
@@ -0,0 +1,25 @@
+import httpService from '../../services/httpService';
+import changeCase from 'change-object-case';
+import PropTypes from 'prop-types';
+import { deserialize } from './story';
+
+export const getStories = async (projectId, startDate, endDate) => {
+    const params = serialize({startDate, endDate});
+    const { data } = await httpService
+        .get(`/project_boards/${projectId}/iterations`, { params });
+    return data.stories.map(story => deserialize(story.story));
+};
+
+export const pastIterationPropTypesShape = PropTypes.shape({
+    startDate: PropTypes.string,
+    endDate: PropTypes.string,
+    iterationNumber: PropTypes.number,
+    stories: PropTypes.array,
+    points: PropTypes.number
+});
+
+const serialize = (data) =>
+    changeCase.snakeKeys(data, {
+        recursive: true,
+        arrayRecursive: true
+    });
