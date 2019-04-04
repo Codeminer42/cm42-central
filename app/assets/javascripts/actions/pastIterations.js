420e3fc0eadb697c055dd3926131965fe4ea9293
jprask <jpraskys@gmail.com>
2019-04-04 17:53:26 -0300

    Create past story request actions

--- app/assets/javascripts/actions/pastIterations.js
+++ app/assets/javascripts/actions/pastIterations.js
@@ -4,3 +4,38 @@ export const receivePastIterations = (pastIterations) => ({
   type: actionTypes.RECEIVE_PAST_ITERATIONS,
   data: pastIterations
 });
+
+export const requestPastStories = iterationNumber => ({
+  type: actionTypes.REQUEST_PAST_STORIES,
+  iterationNumber
+});
+
+export const receivePastStories = (stories, iterationNumber) => ({
+  type: actionTypes.RECEIVE_PAST_STORIES,
+  iterationNumber,
+  stories
+});
+
+export const errorRequestPastStories = error => ({
+  type: actionTypes.ERROR_REQUEST_PAST_STORIES,
+  error
+}); 
+
+export const fetchPastStories = (iterationNumber, startDate, endDate) => 
+  async (dispatch, getState, { PastIteration }) => {
+    const { project } = getState();
+
+    dispatch(requestPastStories(iterationNumber));
+
+    try {
+      const stories = await PastIteration.getStories(
+        project.id,
+        startDate,
+        endDate
+      );
+
+      dispatch(receivePastStories(stories, iterationNumber));
+    } catch (error) {
+      dispatch(errorRequestPastStories(error, iterationNumber));
+    }
+  };
