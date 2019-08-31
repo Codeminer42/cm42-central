b9e635a7579efbb62e1731d24c5b3d9403108643
Neto Chaves <netobac1@gmail.com>
2019-08-31 16:19:23 -0300

    date formatting

--- app/assets/javascripts/components/stories/History.js
+++ app/assets/javascripts/components/stories/History.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const History = (props) => {
-  const result = props.history.map(item => {
+const History = ({ history }) => {
+  const result = history.map(item => {
     return (
       <div key={item.activity.id} className="history-activity">
         <Header
@@ -10,34 +11,61 @@ const History = (props) => {
         />
         <Changes changes={item.activity.subject_changes} />
       </div>
-    )
-  })
-  return <div className="History"> {result} </div>
-}
+    );
+  });
+  return <div className="History"> {result} </div>;
+};
 
-export const Header = props =>
+export const Header = ({ title, date }) => (
   <div className="header">
-    <div className="title">{props.title}</div>
-    <div className="date">{props.date}</div>
+    <div className="title">{title}</div>
+    <div className="date">{I18n.l("date.formats.short", date)}</div>
   </div>
+);
 
+export const getDate = (key, changes) => {
+  if (key.endsWith("_at")) {
+    return [
+      changes[key][0] ? I18n.l("date.formats.long", changes[key][0]) : null,
+      changes[key][0] ? I18n.l("date.formats.long", changes[key][1]) : null
+    ];
+  }
+  return [changes[key][0], changes[key][1]];
+};
 
-export const Changes = props => {
-  const result = Object.keys(props.changes).map(key => {
+export const Changes = ({ changes }) => {
+  const result = Object.keys(changes).map(key => {
+    const value = getDate(key, changes);
     return (
       <div key={key} className="info">
-        <div className="action">{I18n.t(`activerecord.attributes.story.${key}`)}</div>
+        <div className="action">
+          {I18n.t(`activerecord.attributes.story.${key}`)}
+        </div>
         <div className="changes">
-          <div> {props.changes[key][0] || '---'} </div>
-          <div> <i className="mi md-18">arrow_forward</i> </div>
-          <div> {props.changes[key][1] || '---'} </div>
+          <div> {value[0] || "---"} </div>
+          <div>
+            <i className="mi md-18">arrow_forward</i>
+          </div>
+          <div> {value[1] || "---"} </div>
         </div>
       </div>
-    )
-  })
+    );
+  });
 
-  return <div> {result} </div>
+  return <div> {result} </div>;
+};
 
-}
+History.propTypes = {
+  history: PropTypes.array.isRequired
+};
 
-export default History
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired
+};
+
+Changes.propTypes = {
+  changes: PropTypes.object.isRequired
+};
+
+export default History;
