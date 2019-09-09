ab039b0b450798eb88294f02628a27581e9d3d29
edumoreira1506 <contato@eduardoem.com.br>
2019-09-09 09:40:20 -0300

    Create new file for project board tests

--- spec/javascripts/actions/history_spec.js
+++ spec/javascripts/actions/history_spec.js
@@ -58,26 +58,4 @@ describe('History Actions', () => {
     });
     expect(fakeDispatch).toHaveBeenCalledWith(actions.errorLoadHistory());
   });
-
-  it('Should dispatch toggleHistory when storyId is true', async () => {
-    const storyId = 127;
-
-    const fakeGetHash = sinon.stub().returns(storyId);
-
-    const fakeDispatch = sinon.stub().resolves({});
-
-    await expandStoryIfNeeded(fakeDispatch, fakeGetHash);
-    expect(fakeDispatch).toHaveBeenCalledWith(toggleStory(storyId));
-  })
-
-  it('Should not dispatch toggleHistory when storyId is false', async () => {
-    const storyId = null;
-
-    const fakeGetHash = sinon.stub().returns(storyId);
-
-    const fakeDispatch = sinon.stub().resolves({});
-
-    await expandStoryIfNeeded(fakeDispatch, fakeGetHash);
-    expect(fakeDispatch).toHaveBeenCalledWith(toggleStory(storyId));
-  })
 });
