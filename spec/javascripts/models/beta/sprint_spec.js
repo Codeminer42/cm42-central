47c653dd1f98d75d9865bf5806dd3a7f1b23e7d8
edumoreira1506 <contato@eduardoem.com.br>
2019-10-02 11:31:16 -0300

    Tests for Sprint.sort

---
+++ spec/javascripts/models/beta/sprint_spec.js
@@ -0,0 +1,54 @@
+import * as Sprint from 'models/beta/sprint';
+
+describe('Sprint model', () => {
+  const doneSprint = { hasStories: '' }
+  const undoneSprint = {}
+
+  describe('sort', () => {
+    describe('when is done sprints', () => {
+      const amountOfSprints = [1,10,100]
+
+      amountOfSprints.forEach(amount => {
+        describe(`when have ${amount} sprints`, () => {
+          let doneSprints = Array(amount).fill(doneSprint);
+          let changedSprints;
+
+          beforeEach(() => {
+            changedSprints = Sprint.sort(doneSprints);
+          });
+
+          it('return reverse order', () => {
+            expect(changedSprints).toEqual(doneSprints.reverse());
+          });
+    
+          it(`return ${doneSprints.length} sprints`, () => {
+            expect(changedSprints.length).toEqual(doneSprints.length);
+          });
+        })
+      })
+    });
+
+    describe('when is undone sprint', () => {
+      const amountOfSprints = [1,10,100]
+
+      amountOfSprints.forEach(amount => {
+        describe(`when have ${amount} sprints`, () => {
+          let undoneSprints = Array(amount).fill(undoneSprint);
+          let changedSprints;
+    
+          beforeEach(() => {
+            changedSprints = Sprint.sort(undoneSprints);
+          });
+    
+          it('return the same order', () => {
+            expect(changedSprints).toEqual(changedSprints)
+          });
+    
+          it(`return ${undoneSprints.length} sprints`, () => {
+            expect(changedSprints.length).toEqual(undoneSprints.length);
+          });
+        });
+      });
+    });
+  });
+});
