bcd23ed133e4fdb3e38c8d71059b54a4c65fda99
edumoreira1506 <contato@eduardoem.com.br>
2019-10-08 10:47:38 -0300

    Component for search

---
+++ app/assets/javascripts/components/stories/StorySearch.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { search } from "actions/story";
+import { connect } from "react-redux";
+
+const searchDiv = document.getElementById('portal-search-input');
+
+export class StorySearch extends Component {
+  constructor(props) {
+    super(props);
+
+    const { projectId, search } = props;
+
+    this.state = {
+      keyWord: '',
+      searchedStories: [],
+      projectId,
+      search
+    }
+  }
+  
+  handleChange = e => {
+    this.setState({ keyWord: e.target.value })
+  }
+
+  handleSearch = async (e) => {
+    e.preventDefault()
+
+    const { keyWord, projectId, search } = this.state
+
+    search(keyWord, projectId)
+  }
+
+  render() {
+    const { keyWord } = this.state;
+
+    return ReactDOM.createPortal(
+      <form 
+        action="#" 
+        acceptCharset="UTF-8" 
+        method="post"
+        onSubmit={this.handleSearch}
+      >
+        <div className="form-group">
+          <input 
+            type="text" 
+            onChange={this.handleChange} 
+            placeholder="Search" 
+            className="form-control input-sm"
+            value={keyWord}
+          />
+        </div>
+      </form>,
+      searchDiv
+    );
+  }
+}
+
+StorySearch.propTypes = {
+  projectId: PropTypes.number.isRequired
+};
+
+const mapStateToProps = ({ project }) => ({ projectId: project.id });
+
+export default connect(
+  mapStateToProps,
+  {
+    search
+  }
+)(StorySearch);
