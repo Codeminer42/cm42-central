a2c486c4b2a0898ac6d0f03edaa62ccc9873054b
edumoreira1506 <contato@eduardoem.com.br>
2019-10-10 11:53:30 -0300

    Changed id for data-portal

--- app/assets/javascripts/portals/PortalSearchInput.js
+++ app/assets/javascripts/portals/PortalSearchInput.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 
 const PortalSearchInput = ({ children }) => ReactDOM.createPortal(
   children,
-  document.getElementById('portal-search-input'),
+  document.querySelector('[data-portal="portal-search-input"]')
 );
 
 export default PortalSearchInput;
