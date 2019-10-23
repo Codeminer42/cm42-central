4faa5eb440fef9340174e8f5f86b395a11a9d4ef
edumoreira1506 <contato@eduardoem.com.br>
2019-10-23 10:57:06 -0300

    Added style for background

--- app/assets/javascripts/components/story/StoryItem.js
+++ app/assets/javascripts/components/story/StoryItem.js
@@ -4,17 +4,18 @@ import CollapsedStory from './CollapsedStory';
 import ExpandedStory from './ExpandedStory';
 import { connect } from 'react-redux';
 import { toggleStory } from '../../actions/story';
-import { releaseIsLate, isHighlighted } from '../../models/beta/story';
+import { releaseIsLate, isHighlighted, isAccepted } from '../../models/beta/story';
 
 export const StoryItem = ({ story, toggleStory, from }) => {
-  const className = releaseIsLate(story) ? 'Story--late-release' : '';
+  const releaseClass = releaseIsLate(story) ? 'Story--late-release' : '';
   const title = releaseIsLate(story) ? I18n.t('story.warnings.backlogged_release') : '';
-  const styleFocus = isHighlighted(story) ? 'Story--highlighted' : '';
+  const highlightClass = isHighlighted(story) ? 'Story--highlighted' : '';
+  const acceptedClass = isAccepted(story) ? 'Story--accepted' : '';
 
   const childProps = {
     story,
     onToggle: () => toggleStory(story.id, from),
-    className: `${className} ${styleFocus}`,
+    className: `${releaseClass} ${highlightClass} ${acceptedClass}`,
     title,
     from
   }
