f071d8ada9326ac26abf721af1e152028abe7604
Gabriel Baldao <gabrielbaldao@gmail.com>
2019-11-04 10:49:38 -0300

    improve describe of the tests

--- spec/models/story_search_spec.rb
+++ spec/models/story_search_spec.rb
@@ -88,37 +88,37 @@ describe StorySearch do
       expect(StorySearch.new(project.stories, 'abc').search_labels).to eq([@story3, @story4])
     end
 
-    describe 'when try do the search with valid labels' do
-      context 'when the single label used is valid' do
+    describe 'when try do the search with valid operands' do
+      context 'when the single operand used is valid' do
         subject { StorySearch.new(project.stories, 'title: HELL') }
 
-        it 'should returns array with HELL and HELLO stories' do
+        it 'should return all the stories that match the operand' do
           expect(subject.search).to eq([@story1, @story3])
         end
       end
 
-      context 'when all labels used are valid' do
+      context 'when all operands used are valid' do
         subject { StorySearch.new(project.stories, 'title: HELL, labels: foo') }
 
-        it 'should returns HELL story' do
+        it 'should return only the stories that match all operands at the same time' do
           expect(subject.search).to eq([@story1])
         end
       end
     end
 
-    describe 'when try do the search with wrong labels' do
-      context 'when the single label used is invalid' do
+    describe 'when try do the search with wrong operands' do
+      context 'when the single operand used is invalid' do
         subject { StorySearch.new(project.stories, 'wrong: HELL') }
 
-        it 'should returns empty array' do
+        it 'should return no results' do
           expect(subject.search).to eq([])
         end
       end
 
-      context 'when one of the labels are valid and the other is invalid' do
+      context 'when one of the operands are valid and the other is invalid' do
         subject { StorySearch.new(project.stories, 'title: HELL, wrong: WOR') }
 
-        it 'should returns empty array' do
+        it 'should return no results' do
           expect(subject.search).to eq([])
         end
       end
