893f09292ddd950cb4c143ed2dc0a203ee4b89ca
Mauricio P <mauriciopdvg@gmail.com>
2019-11-06 10:53:08 -0300

    Migrate integrations tests

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,3 +1,8 @@
 class Integration < ApplicationRecord
-  include Central::Support::IntegrationConcern
+  VALID_INTEGRATIONS = %w[discord mattermost slack gitlab].freeze
+
+  belongs_to :project
+  validates :project, presence: true
+  validates :kind, inclusion: { in: VALID_INTEGRATIONS }, presence: true
+  validates :data, presence: true
 end
