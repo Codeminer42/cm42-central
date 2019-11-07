96c1e77a5d4f20b3a76838136b53f80dd7c1f551
Mauricio P <mauriciopdvg@gmail.com>
2019-11-07 09:42:55 -0300

    Migrate user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,65 @@
 require 'rails_helper'
 
 describe User do
+  describe 'validations' do
+    it 'requires a name' do
+      subject.name = ''
+      subject.valid?
+      expect(subject.errors[:name].size).to eq(1)
+    end
+
+    it 'requires initials' do
+      subject.initials = ''
+      subject.valid?
+      expect(subject.errors[:initials].size).to eq(1)
+    end
+
+    it { is_expected.to validate_inclusion_of(:role).in_array(User::ROLES) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:enrollments) }
+    it { is_expected.to have_many(:teams) }
+    it { is_expected.to have_many(:memberships) }
+    it { is_expected.to have_many(:projects) }
+
+    describe 'validate distinctiveness of projects through memberships' do
+      let(:user) { create :user }
+      let(:project) { create :project }
+
+      before do
+        create :membership, user: user, project: project
+      end
+
+      it "doesn't allow the same project in two different memberships" do
+        expect do
+          create :membership, user: user, project: project
+        end.to raise_error(ActiveRecord::RecordNotUnique)
+      end
+    end
+  end
+
+  describe '#remove_story_association' do
+    let(:user) { create :user }
+    let(:project) { build :project }
+    let(:story) { build :story, project: project }
+
+    before do
+      project.users << user
+      project.save
+      story.owned_by = user
+      story.requested_by = user
+      story.save
+    end
+
+    it 'removes the story owner and requester when the user is destroyed' do
+      expect { user.destroy }.to change { Membership.count }.by(-1)
+      story.reload
+      expect(story.owned_by).to be_nil
+      expect(story.requested_by).to be_nil
+    end
+  end
+
   describe '#to_s' do
     subject do
       build(:user, name: 'Dummy User', initials: 'DU',
