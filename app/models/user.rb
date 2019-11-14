ec612181b5bb9b7c8410ff629653fdc0a8c3577b
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-11-14 13:59:14 -0300

    Migrate user tests (#609)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,47 @@
 class User < ApplicationRecord
-  include Central::Support::UserConcern::Associations
-  include Central::Support::UserConcern::Validations
-  include Central::Support::UserConcern::Callbacks
+  # Flag used to identify if the user was found or created from find_or_create
+  attr_accessor :was_created
+  attr_accessor :team_slug
+
+  JSON_ATTRIBUTES = %w[id name initials username email finished_tour].freeze
+  AUTHENTICATION_KEYS = %i[email].freeze
+  ROLES = %w[manager developer guest].freeze
+
+  before_validation :set_random_password_if_blank
+
+  after_save :set_team
+
+  before_destroy :remove_story_association
+
+  has_many :enrollments
+  has_many :teams, through: :enrollments
+  has_many :memberships, dependent: :destroy
+  has_many :projects, -> { distinct }, through: :memberships do
+    def not_archived
+      where(archived_at: nil)
+    end
+  end
+
+  validates :name, :username, :initials, presence: true
+  validates :username, uniqueness: true
+
+  extend Enumerize
+  extend ActiveModel::Naming
+  enumerize :role, in: ROLES
 
   include Gravtastic
   gravtastic default: 'identicon'
 
   # FIXME: - DRY up, repeated in Story model
-  JSON_ATTRIBUTES = %w[id name initials username email finished_tour].freeze
-
-  AUTHENTICATION_KEYS = %i[email].freeze
-
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :authy_authenticatable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
-         authentication_keys:   AUTHENTICATION_KEYS,
+         authentication_keys: AUTHENTICATION_KEYS,
          strip_whitespace_keys: AUTHENTICATION_KEYS,
-         confirmation_keys:     AUTHENTICATION_KEYS,
-         reset_password_keys:   AUTHENTICATION_KEYS
+         confirmation_keys: AUTHENTICATION_KEYS,
+         reset_password_keys: AUTHENTICATION_KEYS
   # unlock_keys: AUTHENTICATION_KEYS
-
-  # Flag used to identify if the user was found or created from find_or_create
-  attr_accessor :was_created
 
   scope :recently_created, lambda { |created_at|
     where('users.created_at > ?', created_at) if created_at
@@ -84,5 +103,26 @@ class User < ApplicationRecord
   def team_from_project(project)
     user_team_ids = teams.pluck(:id)
     project.teams.find_by(id: user_team_ids)
+  end
+
+  def set_random_password_if_blank
+    if new_record? && password.blank? && password_confirmation.blank?
+      self.password = self.password_confirmation = Digest::SHA1.hexdigest(
+        "--#{Time.current}--#{email}--"
+      )[0, 8]
+    end
+  end
+
+  def set_team
+    if team_slug
+      team = Team.not_archived.find_by(slug: team_slug)
+      enrollments.create(team: team) if team
+    end
+  end
+
+  def remove_story_association
+    Story.where(requested_by_id: id).update_all(requested_by_id: nil, requested_by_name: nil)
+    Story.where(owned_by_id: id).update_all(owned_by_id: nil, owned_by_name: nil)
+    Membership.where(user_id: id).delete_all
   end
 end
