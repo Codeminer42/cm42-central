be292c2b8881a73683e31f3779740f5f4a454a96
Gabriel Baldão <gabriel_cbaldao@hotmail.com>
2019-11-27 13:10:21 -0300

    Migrate story tests (#611)
    * migrate story tests
    
    * add validators

---
+++ app/validators/central_estimate_validator.rb
@@ -0,0 +1,11 @@
+class CentralEstimateValidator < ActiveModel::EachValidator
+  # TODO: Change the name to EstimateValidator when remove support gem
+  # Checks that the estimate being validated is valid for record.project
+  def validate_each(record, attribute, value)
+    if record.project
+      unless record.project.point_values.include?(value)
+        record.errors[attribute] << 'is not an allowed value for this project'
+      end
+    end
+  end
+end
