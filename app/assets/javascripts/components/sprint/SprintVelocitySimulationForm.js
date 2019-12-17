fad19000335f9525c27987ff363d013ad1b383d7
felipebrunetti94 <felipe.brunetti94@gmail.com>
2019-12-17 14:06:25 -0300

    internationalize simulation form

--- app/assets/javascripts/components/sprint/SprintVelocitySimulationForm.js
+++ app/assets/javascripts/components/sprint/SprintVelocitySimulationForm.js
@@ -16,14 +16,16 @@ export const SprintVelocitySimulationForm = ({
     <div className="velocity-change">
       <div className="form-inline velocity-form">
         <div className="form-group">
-          <label className="sr-only" htmlFor="simulatedSprintVelocity">{currentSprintVelocity}</label>
+          <label className="sr-only" htmlFor="simulatedSprintVelocity">
+            {I18n.t('override.velocity')}
+          </label>
           <input
             type="number"
             min="1"
             className="form-control input-sm velocity-change-input"
             id="simulatedSprintVelocity"
             size="2"
-            placeholder={currentSprintVelocity}
+            placeholder={I18n.t('override.velocity')}
             value={sprintVelocityToSimulate}
             onChange={ e => setSprintVelocityToSimulate(e.target.value)}
           />
@@ -34,17 +36,21 @@ export const SprintVelocitySimulationForm = ({
             value="revert"
             className="btn btn-default btn-sm velocity-change-btn"
             onClick={revertToCalculatedVelocity}
-          >revert</button>
+          >
+            {I18n.t('revert')}
+          </button>
           <button
             name="apply"
             value="apply"
             className="btn btn-primary btn-sm velocity-change-btn"
-            onClick={() => simulateSprintVelocity(sprintVelocityToSimulate)}>apply
+            onClick={() => simulateSprintVelocity(sprintVelocityToSimulate)}
+          >
+            {I18n.t('apply')}
           </button>
         </div>
       </div>
-      <p className="velocity-change-notice">You can simulate with different velocity values.</p>
-      <p className="velocity-change-notice">The iterations will be temporatily recalculated to reflect the simulated velocity but the real Velocity will still be the average of the last 3 iterations.</p>
+      <p className="velocity-change-notice">{I18n.t('override.explanation_1')}</p>
+      <p className="velocity-change-notice">{I18n.t('override.explanation_2')}</p>
     </div>
   )
 };
