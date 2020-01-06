40ec3b2960fc28a29695092ad0f741d0f940e516
felipebrunetti94 <felipe.brunetti94@gmail.com>
2020-01-06 10:18:16 -0300

    refact for better reading

--- spec/javascripts/reducers/notification_spec.js
+++ spec/javascripts/reducers/notification_spec.js
@@ -2,37 +2,51 @@ import actionsTypes from "../../../app/assets/javascripts/actions/actionTypes";
 import reducer from "../../../app/assets/javascripts/reducers/notifications";
 
 describe("Notification Reducer",() => {
-  it("should return initialState", () => {
-    expect(reducer(undefined, [])).toEqual([]);
+  describe("DEFAULT", () => {
+    it("return the initialState", () => {
+      const initialState = [];
+      expect(reducer(undefined, initialState)).toEqual([]);
+    });
+  })
+
+describe('ADD_NOTIFICATION', () => {
+    it('adds a notification to the state',() => {
+      const state = [
+        { id: 1 },
+        { id: 2 }
+      ];
+
+      const action = {
+        type: actionsTypes.ADD_NOTIFICATION,
+        notification: { id: 3 }
+      };
+
+      const expectedState = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 }
+      ];
+
+      expect(reducer(state, action)).toEqual(expectedState);
+    });
   });
-  it("should add notification to the state ", () => {
-    const state = [
-      { id: 1 },
-      { id: 2 }
-    ];
 
-    const action = {
-      type: actionsTypes.ADD_NOTIFICATION,
-      notification: { id: 3 }
-    };
+  describe('REMOVE_NOTIFICATION', () => {
+    it('removes a notification from the state', () => {
+      const state = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 }
+      ];
 
-    const expectedNotificationsState = [action.notification, ...state];
+      const action = {
+        type: actionsTypes.REMOVE_NOTIFICATION,
+        id: 2
+      };
 
-    expect(reducer(state, action)).toEqual(expectedNotificationsState);
-  });
-  it("should remove notification from the state", () => {
-    const state = [
-      { id: 1 },
-      { id: 2 },
-      { id: 3 }
-    ];
+      const expectedState = [{ id: 1 }, { id: 3 }];
 
-    const expectedNotificationsArray = [{ id: 1 }, { id: 3 }];
-
-    const action = {
-      type: actionsTypes.REMOVE_NOTIFICATION,
-      id: 2
-    };
-    expect(reducer(state, action)).toEqual(expectedNotificationsArray);
+      expect(reducer(state, action)).toEqual(expectedState);
+    })
   });
 })
