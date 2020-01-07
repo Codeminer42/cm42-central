3d33fa07ca50439682acd19b946f4e49abe37997
felipebrunetti94 <felipe.brunetti94@gmail.com>
2020-01-07 10:42:08 -0300

    add tests for reducer/pastIterations.js

---
+++ spec/javascripts/reducers/pastIterations_spec.js
@@ -0,0 +1,128 @@
+import actionTypes from 'actions/actionTypes';
+import reducer from "../../../app/assets/javascripts/reducers/pastIterations";
+
+describe("Past Iterations Reducer", () => {
+  describe("RECEIVE_PAST_ITERATIONS", () => {
+    it("return formatted past iterations,", () => {
+      const state = [];
+
+      const action = {
+        type: actionTypes.RECEIVE_PAST_ITERATIONS,
+        data: [{
+          hasStories: true,
+          iterationNumber: 1,
+          stories: []
+        }]
+      };
+
+      expect(reducer(state, action)).toEqual([{
+        hasStories: true,
+        iterationNumber: 1,
+        error: null,
+        storyIds: [],
+        fetched: false,
+        isFetching: false,
+        stories: undefined
+      }]);
+    });
+  });
+
+  describe("REQUEST_PAST_STORIES", () => {
+    it("add isFetching: true, to the selected iteration ", () => {
+      const state = [
+        {
+          iterationNumber: 1
+        },
+        {
+          iterationNumber: 2
+        }
+      ];
+
+      const action = {
+        type: actionTypes.REQUEST_PAST_STORIES,
+        iterationNumber: 1,
+      };
+
+      expect(reducer(state, action)).toEqual([
+        {
+          iterationNumber: 1,
+          isFetching: true
+        },
+        {
+          iterationNumber: 2
+        }
+      ]);
+    });
+  });
+
+  describe("RECEIVE_PAST_STORIES", () => {
+    it("return formatted past stories", () => {
+      const state = [
+        {
+          iterationNumber: 1
+        },
+        {
+          iterationNumber: 2
+        }
+      ];
+
+      const action = {
+        type: actionTypes.RECEIVE_PAST_STORIES,
+        iterationNumber: 1,
+        stories:[
+          {id: 1},
+          {id: 2}
+        ],
+        from: undefined
+      };
+
+      expect(reducer(state, action)).toEqual([
+        {
+          iterationNumber: 1,
+          fetched: true,
+          isFetching: false,
+          storyIds: [ 1, 2 ]
+        },
+        { iterationNumber: 2}
+      ]);
+    });
+  });
+
+
+  describe("ERROR_REQUEST_PAST_STORIES", () => {
+    it("", () => {
+      const state = [
+        {
+          iterationNumber: 1
+        },
+        {
+          iterationNumber: 2
+        }
+      ];
+
+      const action = {
+        type: actionTypes.ERROR_REQUEST_PAST_STORIES,
+        iterationNumber: 1,
+        error: Error('whoopsie')
+      };
+
+      expect(reducer(state, action)).toEqual([
+        {
+          iterationNumber: 1,
+          fetched: false,
+          isFetching: false,
+          error: Error('whoopsie')
+        },
+        {
+          iterationNumber: 2
+        }
+      ]);
+    });
+  });
+
+  describe("DEFAULT", () => {
+    it("return initialState", () => {
+      expect(reducer(undefined, {})).toEqual([]);
+    });
+  });
+});
