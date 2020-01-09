f7e40b7e76b5c0e236e91c9ba2c31ffed56b4f4e
felipebrunetti94 <felipe.brunetti94@gmail.com>
2020-01-09 17:47:34 -0300

    adds test for project.js reducer

---
+++ spec/javascripts/reducers/project_spec.js
@@ -0,0 +1,36 @@
+import actionsTypes from "../../../app/assets/javascripts/actions/actionTypes";
+import reducer from "../../../app/assets/javascripts/reducers/project";
+
+describe("Project Reducer",() => {
+  describe("RECEIVE_PROJECT",() => {
+    it("returns action.data",() => {
+      const state = {};
+
+      const action = {
+        type: actionsTypes.RECEIVE_PROJECT,
+        data: {id: 1}
+      };
+
+      expect(reducer(state, action)).toEqual(action.data);
+    });
+  });
+
+  describe("ADD_LABEL_TO_PROJECT",() => {
+    it("adds new label to labels",() => {
+      const state = { labels: []};
+
+      const action = {
+        type: actionsTypes.ADD_LABEL_TO_PROJECT,
+        label: {name: "label"}
+      };
+
+      expect(reducer(state, action)).toEqual({ labels: [{  name: "label"}]});
+    });
+  });
+
+  describe("DEFAULT",() => {
+    it("returns the initialState",() => {
+      expect(reducer(undefined, {})).toEqual({});
+    });
+  });
+});
