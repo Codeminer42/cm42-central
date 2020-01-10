b310b5de3b176e78852547ce86e6f94d7256bd31
felipebrunetti94 <felipe.brunetti94@gmail.com>
2020-01-10 09:47:08 -0300

    fix coding style

--- spec/javascripts/reducers/project_spec.js
+++ spec/javascripts/reducers/project_spec.js
@@ -3,12 +3,12 @@ import reducer from "../../../app/assets/javascripts/reducers/project";
 
 describe("Project Reducer",() => {
   describe("RECEIVE_PROJECT",() => {
-    it("returns action.data",() => {
+    it("returns data from action",() => {
       const state = {};
 
       const action = {
         type: actionsTypes.RECEIVE_PROJECT,
-        data: {id: 1}
+        data: { id: 1 }
       };
 
       expect(reducer(state, action)).toEqual(action.data);
@@ -17,14 +17,14 @@ describe("Project Reducer",() => {
 
   describe("ADD_LABEL_TO_PROJECT",() => {
     it("adds new label to labels",() => {
-      const state = { labels: []};
+      const state = { labels: [] };
 
       const action = {
         type: actionsTypes.ADD_LABEL_TO_PROJECT,
-        label: {name: "label"}
+        label: { name: "label" }
       };
 
-      expect(reducer(state, action)).toEqual({ labels: [{  name: "label"}]});
+      expect(reducer(state, action)).toEqual({ labels: [{ name: "label"}]});
     });
   });
 
