b433308db25e05658adb01372cd8525bb4f146ec
felipebrunetti94 <felipe.brunetti94@gmail.com>
2020-01-13 10:46:39 -0300

    add new case tests

--- spec/javascripts/reducers/notification_spec.js
+++ spec/javascripts/reducers/notification_spec.js
@@ -9,8 +9,24 @@ describe("Notification Reducer",() => {
     });
   })
 
-describe('ADD_NOTIFICATION', () => {
+  describe('ADD_NOTIFICATION', () => {
+    let state
+    beforeEach(() => {
+      state = [
+        { id: 1 },
+        { id: 2 }
+      ];
+    })
     it('adds a notification to the state',() => {
+      const action = {
+        type: actionsTypes.ADD_NOTIFICATION,
+        notification: { id: 3 }
+      };
+
+      expect(reducer(state, action)).toEqual([{ id: 3 }, { id: 1 }, { id: 2 }]);
+    });
+
+    it('adds multiple notifications to the state',() => {
       const state = [
         { id: 1 },
         { id: 2 }
@@ -18,10 +34,10 @@ describe('ADD_NOTIFICATION', () => {
 
       const action = {
         type: actionsTypes.ADD_NOTIFICATION,
-        notification: { id: 3 }
+        notification: [{ id: 3 }, { id: 4 }, { id: 5 }]
       };
 
-      expect(reducer(state, action)).toEqual([{ id: 3 }, { id: 1 }, { id: 2 }]);
+      expect(reducer(state, action)).toEqual([{ id: 3 }, { id: 4 }, { id: 5 }, { id: 1 }, { id: 2 }]);
     });
   });
 
