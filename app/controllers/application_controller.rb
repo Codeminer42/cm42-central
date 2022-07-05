477e20159d8d426ffba3493ca673fb623f7386c1
Lucas Assis <lucas.assis@codeminer42.com>
2022-07-05 11:09:16 -0300

    Fix deprecation warnings for Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
 
-  include Pundit
+  include Pundit::Authorization
   include SidebarController
   include Renderers::CSV
 
