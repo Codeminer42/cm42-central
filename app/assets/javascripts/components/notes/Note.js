90371a4549bcc0d7f552f255d38b618146bdc847
grazirs <graziribeiro95@gmail.com>
2022-10-27 11:51:25 -0300

    refactor: update Note class component to functional

--- app/assets/javascripts/components/notes/Note.js
+++ app/assets/javascripts/components/notes/Note.js
@@ -1,49 +1,35 @@
 import React from 'react';
 
-class Note extends React.Component {
-  constructor(props) {
-    super(props);
-    this._handleDelete = this._handleDelete.bind(this);
+const Note = ({ note, disabled, handleDelete }) => {
+
+  const _handleDelete = () => {
+    handleDelete(note)
   }
 
-  _handleDelete() {
-    const { note, handleDelete } = this.props;
-    handleDelete(note);
-  }
-
-  parseNote(note) {
+  const parseNote = () => {
     return ({
       __html: window.md.makeHtml(note.escape('note'))
     });
   }
-
-  renderDelete() {
-    return (
-      <span>
-        - <span
-            onClick={this._handleDelete}
+  return (
+    <div className='note'>
+      <div dangerouslySetInnerHTML={parseNote(note)}></div>
+      <div className='note_meta'>
+        <span className='user'> { note.attributes.user_name } </span> -
+        <span className='created_at'> { note.attributes.created_at } </span>
+        {!disabled && <span>
+          - <span
+            onClick={_handleDelete}
             title={I18n.t('delete')}
             className='delete-btn delete-note'
+            data-testid='delete-btn'
           >
-          { I18n.t('delete') }
-        </span>
-      </span>
-    );
-  }
-
-  render() {
-    const { note, disabled } = this.props;
-    return (
-      <div className="note">
-        <div dangerouslySetInnerHTML={this.parseNote(note)}></div>
-        <div className="note_meta">
-          <span className="user"> { note.get('user_name') } </span> -
-          <span className="created_at"> { note.get('created_at') } </span>
-          { !disabled && this.renderDelete() }
-        </div>
+            {I18n.t('delete')}
+          </span>
+        </span>}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Note;
