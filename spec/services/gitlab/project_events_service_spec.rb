f214a75c188e24dd73e71ed7e251195791a98c40
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-04-17 15:51:17 -0300

    fix: gitlab service use of StoryOperations::Update

--- spec/services/gitlab/project_events_service_spec.rb
+++ spec/services/gitlab/project_events_service_spec.rb
@@ -6,13 +6,15 @@ describe Gitlab::ProjectEventsService do
 
     let(:service) { described_class.new(params) }
     let(:story) { FactoryBot.create(:story, :with_project) }
+    let(:story_operations_update_instance) { StoryOperations::Update.new }
 
     before do
       allow(Story).to receive(:find_by).with(branch: 'test-branch').and_return(story)
-      allow(StoryOperations::Update).to receive(:call).with(
-        story,
-        { state: 'delivered' },
-        story.requested_by
+      allow(StoryOperations::Update).to receive(:new).and_return(story_operations_update_instance)
+      allow(story_operations_update_instance).to receive(:call).with(
+        story: story,
+        data: { state: 'delivered' },
+        current_user: story.requested_by
       ).and_return(true)
     end
 
