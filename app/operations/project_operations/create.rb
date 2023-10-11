9b4255170149eee7c35cfd8c7ded28e92200fac1
Sergio Souza <sergiosouzauff@gmail.com>
2023-10-11 14:46:21 -0300

    Move ownership creation to an operation

--- app/operations/project_operations/create.rb
+++ app/operations/project_operations/create.rb
@@ -2,16 +2,17 @@ module ProjectOperations
   class Create
     include Operation
 
-    def initialize(project:, current_user:)
+    def initialize(project:, current_user:, current_team:)
       @project = project
       @current_user = current_user
+      @current_team = current_team
     end
 
     def call
       ActiveRecord::Base.transaction do
         yield save_project
         yield create_activity
-
+        yield create_ownership
         Success(project)
       end
     rescue
@@ -20,7 +21,7 @@ module ProjectOperations
 
     private
 
-    attr_reader :project, :current_user
+    attr_reader :project, :current_user, :current_team
 
     def save_project
       if project.save
@@ -36,6 +37,15 @@ module ProjectOperations
         current_user: current_user,
         action: 'create'
       )
+    end
+
+    def create_ownership
+      begin
+        ownership = current_team.ownerships.create!(project: project, is_owner: true)
+        Success(ownership)
+      rescue StandardError => e
+        Failure(ownership)
+      end
     end
   end
 end
