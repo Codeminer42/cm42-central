e59db1f7f0445d4023357bfc2145c312c277e749
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-10-20 17:06:32 -0300

    feat: update pastIterations reducers to normalize the data

--- app/assets/javascripts/reducers/pastIterations.js
+++ app/assets/javascripts/reducers/pastIterations.js
@@ -1,65 +1,79 @@
 import actionTypes from "actions/actionTypes";
+import {
+  denormalizePastIterations,
+  normalizePastIterations,
+} from "../models/beta/pastIteration";
 
-const initialState = [];
+const initialState = {};
 
 const pastIterationsReducer = (state = initialState, action) => {
+  const denormalizedPastIterations = denormalizePastIterations(state);
+
   switch (action.type) {
     case actionTypes.RECEIVE_PAST_ITERATIONS:
-      return action.data.map((iteration) => {
-        const currentState = state.find(
-          (currentIteration) =>
-            currentIteration.iterationNumber === iteration.iterationNumber
-        );
-        if (currentState?.fetched) {
+      return normalizePastIterations(
+        action.data.map((iteration) => {
+          const currentState = denormalizedPastIterations.find(
+            (currentIteration) =>
+              currentIteration.iterationNumber === iteration.iterationNumber
+          );
+          if (currentState?.fetched) {
+            return {
+              ...iteration,
+              error: null,
+              storyIds: currentState.storyIds,
+              fetched: true,
+              isFetching: false,
+              stories: undefined,
+            };
+          }
           return {
             ...iteration,
             error: null,
-            storyIds: currentState.storyIds,
-            fetched: true,
+            storyIds: [],
+            fetched: false,
             isFetching: false,
             stories: undefined,
           };
-        }
-        return {
-          ...iteration,
-          error: null,
-          storyIds: [],
-          fetched: false,
-          isFetching: false,
-          stories: undefined,
-        };
-      });
+        })
+      );
     case actionTypes.REQUEST_PAST_STORIES:
-      return state.map((iteration) => {
-        if (iteration.iterationNumber === action.iterationNumber) {
-          return { ...iteration, isFetching: true };
-        }
-        return iteration;
-      });
+      return normalizePastIterations(
+        denormalizedPastIterations.map((iteration) => {
+          if (iteration.iterationNumber === action.iterationNumber) {
+            return { ...iteration, isFetching: true };
+          }
+          return iteration;
+        })
+      );
     case actionTypes.RECEIVE_PAST_STORIES:
-      return state.map((iteration) => {
-        if (iteration.iterationNumber === action.iterationNumber) {
-          return {
-            ...iteration,
-            fetched: true,
-            isFetching: false,
-            storyIds: action.stories.map((story) => story.id),
-          };
-        }
-        return iteration;
-      });
+      return normalizePastIterations(
+        denormalizedPastIterations.map((iteration) => {
+          if (iteration.iterationNumber === action.iterationNumber) {
+            return {
+              ...iteration,
+              fetched: true,
+              isFetching: false,
+              storyIds: action.stories.map((story) => story.id),
+            };
+          }
+          return iteration;
+        })
+      );
     case actionTypes.ERROR_REQUEST_PAST_STORIES:
-      return state.map((iteration) => {
-        if (iteration.iterationNumber === action.iterationNumber) {
-          return {
-            ...iteration,
-            fetched: false,
-            isFetching: false,
-            error: action.error,
-          };
-        }
-        return iteration;
-      });
+      return normalizePastIterations(
+        denormalizedPastIterations.map((iteration) => {
+          if (iteration.iterationNumber === action.iterationNumber) {
+            return {
+              ...iteration,
+              fetched: false,
+              isFetching: false,
+              error: action.error,
+            };
+          }
+          return iteration;
+        })
+      );
     default:
       return state;
   }
