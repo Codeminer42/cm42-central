34d58b835ca6aa17eb875035751920b6652ca696
gabrielcicero45 <gabrielcicero45@gmail.com>
2023-11-01 16:38:42 -0300

    Removes project_boards_serializer fix broken tests

--- app/controllers/beta/project_boards_controller.rb
+++ app/controllers/beta/project_boards_controller.rb
@@ -6,24 +6,19 @@ class Beta::ProjectBoardsController < ApplicationController
       project_id: params[:id],
       current_user: current_user,
       current_flow: cookies[:current_flow],
-      projects_scope: policy_scope(Project)
+      projects_scope: policy_scope(Project),
     )
 
     @project = result.success.project
     authorize @project, policy_class: Beta::ProjectPolicy
-    response = result.data.as_json(root: false)
-    render json: collapse_response_stories(response)
+
+    response = result.success.as_json(root: false)
+    render json: response
   end
 
   private
 
   def set_fluid_layout
     @layout_settings[:fluid] = true
-  end
-
-  def collapse_response_stories(response)
-    stories = response['stories']
-    response['stories'] = ProjectBoardsSerializer.collapsed_stories(stories)
-    response
   end
 end
