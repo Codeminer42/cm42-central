b0daf8e899bff9ea8deb765490fd2c6293df3317
Micah Geisel <micah@botandrose.com>
2024-03-31 17:32:16 -0600

    vendor rails-assets-jquery.gritter, so we can remove the defunct rails-assets.org gem source. jquery.gritter hasn't been updated in 11 years, anyways.

---
+++ vendor/gems/rails-assets-jquery.gritter/lib/rails-assets-jquery.gritter.rb
@@ -0,0 +1,47 @@
+require "rails-assets-jquery.gritter/version"
+
+require "rails-assets-jquery"
+
+module RailsAssetsJqueryGritter
+
+  def self.gem_path
+    Pathname(File.realpath(__FILE__)).join('../..')
+  end
+
+  def self.gem_spec
+    Gem::Specification::load(
+      gem_path.join("rails-assets-jquery.gritter.gemspec").to_s
+    )
+  end
+
+  def self.load_paths
+    gem_path.join('app/assets').each_child.to_a
+  end
+
+  def self.dependencies
+    [
+      RailsAssetsJquery
+    ]
+  end
+
+  if defined?(Rails)
+    class Engine < ::Rails::Engine
+      # Rails -> use app/assets directory.
+    end
+  end
+
+end
+
+class RailsAssets
+  @components ||= []
+
+  class << self
+    attr_accessor :components
+
+    def load_paths
+      components.flat_map(&:load_paths)
+    end
+  end
+end
+
+RailsAssets.components << RailsAssetsJqueryGritter
