e1f3843290c4e319ea5c13341b01865cdb852397
Micah Geisel <micah@botandrose.com>
2024-04-01 08:25:34 -0600

    don't shiv our node bin on existing ci tasks.

--- config/initializers/vite.rb
+++ config/initializers/vite.rb
@@ -3,10 +3,12 @@ require "vite_ruby"
 ViteRuby::Runner.prepend Module.new {
   def command_for(args)
     cmd = super
-    if cmd[1] == "node"
-      cmd[1] = "bin/node"
-    else
-      cmd.insert 1, "bin/node"
+    if File.exist?("bin/node")
+      if cmd[1] == "node"
+        cmd[1] = "bin/node"
+      else
+        cmd.insert 1, "bin/node"
+      end
     end
     cmd
   end
