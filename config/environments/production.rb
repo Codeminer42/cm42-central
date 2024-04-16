2c2c6e1d709938fa7aee79c1a1b81dd52ff63011
Micah Geisel <micah@botandrose.com>
2024-04-16 22:08:31 +0100

    prevent empty CDN_URL string from being recognized.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.static_cache_control = "public, max-age=2592000"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['CDN_URL'] if ENV['CDN_URL']
+  config.action_controller.asset_host = ENV['CDN_URL'] if ENV['CDN_URL'].present?
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
