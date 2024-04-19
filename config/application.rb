0e04a3a30b6a5a0ed6595b870580a8ca631a3fe9
Micah Geisel <micah@botandrose.com>
2024-04-19 15:29:58 +0100

    set Rails 5.0 defaults.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Fulcrum
   class Application < Rails::Application
+    config.load_defaults 5.0
+
+    config.active_record.belongs_to_required_by_default = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
