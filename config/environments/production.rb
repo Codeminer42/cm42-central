c2dbf24b435b99d0a464118fdde1dff2d5e3a3cd
Micah Geisel <micah@botandrose.com>
2024-04-21 12:07:23 +0100

    don't log to STDOUT in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  # config.logger = ActiveSupport::Logger.new(STDOUT)
+  #   .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+  #   .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
