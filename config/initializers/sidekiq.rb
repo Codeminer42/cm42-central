c9be58f441362d23f2f20e2bc1aff441acf28f7d
Micah Geisel <micah@botandrose.com>
2024-04-22 22:42:13 +0100

    monkeypatches to get sidekiq 6 compatible with rails 7.1, so we don't have to upgrade to sidekiq 7 and thus redis 6.2 which doesn't have os support in 20.04 or 22.04.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,24 @@ require 'sidekiq'
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDISCLOUD_URL"] }
 end
+Rails.application.config.before_initialize do |app|
+  initializer = app.initializers.find do |initializer|
+    initializer.name == "sidekiq.rails_logger"
+  end
+
+  def initializer.run(*args)
+    # no-op
+  end
+end
+
+Rails.application.config.after_initialize do
+  Sidekiq.configure_server do |config|
+    config[:reloader] = Sidekiq::Rails::Reloader.new
+
+    # This is the integration code necessary so that if a job uses `Rails.logger.info "Hello"`,
+    # it will appear in the Sidekiq console with all of the job context.
+    unless ::Rails.logger == config.logger || ::ActiveSupport::Logger.logger_outputs_to?(::Rails.logger, $stdout)
+      ::Rails.logger = ::ActiveSupport::BroadcastLogger.new(::Rails.logger, config.logger)
+    end
+  end
+end
