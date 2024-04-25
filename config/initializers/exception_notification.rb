7c7f0fdd501bb4e569c20e416a69bcbecfe0bbfe
Micah Geisel <micah@botandrose.com>
2024-04-25 16:00:38 +0100

    try config change for raising all exceptions in sidekiq.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,22 +1,27 @@
 require "exception_notification/rails"
-require "exception_notification/rake"
 require "exception_notification/sidekiq"
 
 ExceptionNotification.configure do |config|
-  # Ignore additional exception types.
-  # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
-  config.ignored_exceptions += %w[
-    ActionController::InvalidAuthenticityToken
-    ActionView::MissingTemplate
-    ActionController::BadRequest
-    ActionDispatch::Http::Parameters::ParseError
-    ActionDispatch::Http::MimeNegotiation::InvalidType
-  ]
+  config.ignored_exceptions = []
 
   # Adds a condition to decide when an exception must be ignored or not.
   # The ignore_if method can be invoked multiple times to add extra conditions.
   config.ignore_if do |exception, options|
     not Rails.env.production?
+  end
+
+  config.ignore_if do |exception, options|
+    %w[
+      ActiveRecord::RecordNotFound
+      AbstractController::ActionNotFound
+      ActionController::RoutingError
+      ActionController::InvalidAuthenticityToken
+      ActionView::MissingTemplate
+      ActionController::BadRequest
+      ActionDispatch::Http::Parameters::ParseError
+      ActionDispatch::Http::MimeNegotiation::InvalidType
+    ].include?(exception.class.to_s) &&
+      !options.dig(:data, :sidekiq)
   end
 
   config.add_notifier :email, {
