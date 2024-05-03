650d20ec5733ddbf8ce4167d8bba59edab67c602
Micah Geisel <micah@botandrose.com>
2024-05-03 23:22:49 +0200

    ignore new incoming form state when story is open.

--- app/javascript/controllers/story_controller.js
+++ app/javascript/controllers/story_controller.js
@@ -3,14 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [
     "checkbox",
+    "permanent",
   ]
 
   open(event) {
     if(event.target.closest(".state-actions")) return
     this.checkboxTarget.checked = true
+    this.permanentTargets.forEach(e => e.setAttribute("data-turbo-permanent", "true"))
   }
 
   close(event) {
     this.checkboxTarget.checked = false
+    this.permanentTargets.forEach(e => e.removeAttribute("data-turbo-permanent"))
   }
 }
