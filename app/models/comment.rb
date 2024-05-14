d85d474b2b816e679c40f7df4435f46eec2d85b9
Micah Geisel <micah@botandrose.com>
2024-05-14 20:38:15 +0200

    comments require at least a body OR an attachment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,11 +6,17 @@ class Comment < ActiveRecord::Base
 
   before_save :cache_user_name
 
-  validates :body, presence: true
-
   delegate :project, to: :story
 
+  validate :body_or_attachment_present
+
   private
+
+  def body_or_attachment_present
+    if body.blank? && attachments.none?
+      errors.add :base, "must have either a body or an attachment"
+    end
+  end
 
   def cache_user_name
     self.user_name = user.name if user.present?
