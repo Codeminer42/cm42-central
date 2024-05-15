6b51e872171e1aa1775ad8198c479a1d88449c99
Micah Geisel <micah@botandrose.com>
2024-05-15 16:44:41 +0200

    using story url should open to story.

---
+++ app/javascript/controllers/open_current_story_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+import useActions from "stimulus-use-actions"
+
+export default class extends Controller {
+  connect() {
+    useActions(this, {
+      window: "hashchange->openCurrentStory",
+    })
+    this.openCurrentStory()
+  }
+
+  openCurrentStory(event) {
+    const location = event
+      ? new URL(event.newURL)
+      : window.location
+    const selector = `${location.hash}-toggle`
+    const toggle = document.querySelector(selector)
+    if(toggle) toggle.checked = true
+  }
+}
+
