d9123614b718fb9ad6c976a7141f5847ed16b540
Gabriel <gabrielmuller97@gmail.com>
2024-08-14 16:46:07 -0300

    remove explicit return from StoryCopyIdClipboard

--- app/assets/javascripts/components/story/StoryCopyIdClipboard.js
+++ app/assets/javascripts/components/story/StoryCopyIdClipboard.js
@@ -1,17 +1,15 @@
-import React from "react";
-import Clipboard from "react-clipboard.js";
+import React from 'react';
+import Clipboard from 'react-clipboard.js';
 
-const StoryCopyIdClipboard = ({ id }) => {
-  return (
-    <Clipboard
-      data-clipboard-text={`#${id}`}
-      component="button"
-      className="story-id"
-      title={I18n.t("story.events.copy_id")}
-    >
-      #{id}
-    </Clipboard>
-  );
-};
+const StoryCopyIdClipboard = ({ id }) => (
+  <Clipboard
+    data-clipboard-text={`#${id}`}
+    component="button"
+    className="story-id"
+    title={I18n.t('story.events.copy_id')}
+  >
+    #{id}
+  </Clipboard>
+);
 
 export default StoryCopyIdClipboard;
