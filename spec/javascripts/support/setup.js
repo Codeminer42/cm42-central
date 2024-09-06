5aa8525cba9b80d579537ba306ca2d8b72d7f606
William Spada <williamrspada02@gmail.com>
2024-09-06 13:22:42 -0300

    chore: readd jest packages and add vitest setupFiles and environment

--- spec/javascripts/support/setup.js
+++ spec/javascripts/support/setup.js
@@ -2,6 +2,7 @@ import 'libs/index';
 import 'raf/polyfill';
 import './enzyme';
 import 'jest-enzyme';
+import 'jest-sinon';
 
 import * as JestDom from '@testing-library/jest-dom';
 
@@ -10,6 +11,6 @@ const { toHaveStyle: toHaveDomStyle, ...jestDomRest } = JestDom;
 expect.extend({ toHaveDomStyle, ...jestDomRest });
 
 window.sinon = require('sinon');
-window.md = { makeHtml: function() {} };
+window.md = { makeHtml: function () {} };
 window.process.env.CLOUDINARY_URL = 'cloudinary://username:password@localhost';
 window.process.env.CLOUDINARY_UPLOAD_PRESET = '';
