d779ab727b4a35b0e98e6c6b78efc93c08480fd5
William Spada <williamrspada02@gmail.com>
2024-09-09 09:41:29 -0300

    test: replace jest-sinon to vitest functionalities on the file iteration_spec.js

--- spec/javascripts/models/iteration_spec.js
+++ spec/javascripts/models/iteration_spec.js
@@ -1,56 +1,52 @@
 import Iteration from 'models/iteration';
 
-describe("iteration", function() {
+describe('iteration', function () {
   let iteration;
 
-  beforeEach(function() {
+  beforeEach(function () {
     iteration = new Iteration({});
   });
 
-  describe("initialize", function() {
-
-    it("should assign stories if passed", function() {
-      var stories = [1,2,3];
-      var iteration = new Iteration({'stories': stories});
+  describe('initialize', function () {
+    it('should assign stories if passed', function () {
+      var stories = [1, 2, 3];
+      var iteration = new Iteration({ stories: stories });
       expect(iteration.get('stories')).toEqual(stories);
     });
-
   });
 
-  describe("defaults", function() {
-
-    it("should have an empty array of stories", function() {
+  describe('defaults', function () {
+    it('should have an empty array of stories', function () {
       expect(iteration.get('stories')).toEqual([]);
     });
-
   });
 
-  describe("points", function() {
+  describe('points', function () {
     let stories;
 
-    beforeEach(function() {
-      var Story = Backbone.Model.extend({name: 'story'});
+    beforeEach(function () {
+      var Story = Backbone.Model.extend({ name: 'story' });
       stories = [
-        new Story({estimate: 2, story_type: 'feature'}),
-        new Story({estimate: 3, story_type: 'feature', state: 'accepted'}),
-        new Story({estimate: 3, story_type: 'bug', state: 'accepted'}) // Only features count
-                                                                       // towards velocity
+        new Story({ estimate: 2, story_type: 'feature' }),
+        new Story({ estimate: 3, story_type: 'feature', state: 'accepted' }),
+        new Story({ estimate: 3, story_type: 'bug', state: 'accepted' }), // Only features count
+        // towards velocity
       ];
-      iteration.set({stories: stories});
+      iteration.set({ stories: stories });
     });
 
-    it("should calculate its points", function() {
+    it('should calculate its points', function () {
       expect(iteration.points()).toEqual(5);
     });
 
-    it("should return 0 for points if it has no stories", function() {
+    it('should return 0 for points if it has no stories', function () {
       iteration.unset('stories');
       expect(iteration.points()).toEqual(0);
     });
 
-    it("should report how many points it overflows by", function() {
+    it('should report how many points it overflows by', function () {
       // Should return 0
-      iteration.set({'maximum_points':2});
+      iteration.set({ maximum_points: 2 });
       var pointsStub = sinon.stub(iteration, 'points');
 
       // Should return 0 if the iteration points are less than maximum_points
@@ -67,25 +63,23 @@ describe("iteration", function() {
       expect(iteration.overflowsBy()).toEqual(3);
     });
 
-    it("should report how many accepted points it has", function() {
+    it('should report how many accepted points it has', function () {
       expect(iteration.acceptedPoints()).toEqual(3);
     });
-
   });
 
-  describe("filling backlog iterations", function() {
-
-    it("should return how many points are available", function() {
-      var pointsStub = sinon.stub(iteration, "points");
+  describe('filling backlog iterations', function () {
+    it('should return how many points are available', function () {
+      var pointsStub = sinon.stub(iteration, 'points');
       pointsStub.returns(3);
 
-      iteration.set({'maximum_points': 5});
+      iteration.set({ maximum_points: 5 });
       expect(iteration.availablePoints()).toEqual(2);
     });
 
-    it("should always accept chores bugs and releases", function() {
+    it('should always accept chores bugs and releases', function () {
       var stub = sinon.stub();
-      var story = {get: stub};
+      var story = { get: stub };
 
       stub.withArgs('story_type').returns('chore');
       expect(iteration.canTakeStory(story)).toBeTruthy();
@@ -95,9 +89,9 @@ describe("iteration", function() {
       expect(iteration.canTakeStory(story)).toBeTruthy();
     });
 
-    it("should not accept anything when isFull is true", function() {
+    it('should not accept anything when isFull is true', function () {
       var stub = sinon.stub();
-      var story = {get: stub};
+      var story = { get: stub };
 
       iteration.isFull = true;
 
@@ -109,14 +103,14 @@ describe("iteration", function() {
       expect(iteration.canTakeStory(story)).toBeFalsy();
     });
 
-    it("should accept a feature if there are enough free points", function() {
-      var availablePointsStub = sinon.stub(iteration, "availablePoints");
+    it('should accept a feature if there are enough free points', function () {
+      var availablePointsStub = sinon.stub(iteration, 'availablePoints');
       availablePointsStub.returns(3);
       var pointsStub = sinon.stub(iteration, 'points');
       pointsStub.returns(1);
 
       var stub = sinon.stub();
-      var story = {get: stub};
+      var story = { get: stub };
 
       stub.withArgs('story_type').returns('feature');
       stub.withArgs('estimate').returns(3);
@@ -129,31 +123,28 @@ describe("iteration", function() {
     });
 
     // Each iteration should take at least one feature
-    it("should always take at least one feature no matter how big", function() {
-      var availablePointsStub = sinon.stub(iteration, "availablePoints");
+    it('should always take at least one feature no matter how big', function () {
+      var availablePointsStub = sinon.stub(iteration, 'availablePoints');
       availablePointsStub.returns(1);
 
       var stub = sinon.stub();
-      var story = {get: stub};
+      var story = { get: stub };
       stub.withArgs('story_type').returns('feature');
       stub.withArgs('estimate').returns(2);
 
       expect(iteration.points()).toEqual(0);
       expect(iteration.canTakeStory(story)).toBeTruthy();
     });
-
-
   });
 
-  describe("isFull flag", function() {
-
-    it("should default to false", function() {
+  describe('isFull flag', function () {
+    it('should default to false', function () {
       expect(iteration.isFull).toEqual(false);
     });
 
-    it("should be set to true once canTakeStory has returned false", function() {
+    it('should be set to true once canTakeStory has returned false', function () {
       var stub = sinon.stub();
-      var story = {get: stub};
+      var story = { get: stub };
 
       iteration.availablePoints = sinon.stub();
       iteration.availablePoints.returns(0);
@@ -167,114 +158,130 @@ describe("iteration", function() {
       expect(iteration.canTakeStory(story)).toBeFalsy();
       expect(iteration.isFull).toEqual(true);
     });
-
   });
 
-  describe("createMissingIterations", function() {
+  describe('createMissingIterations', function () {
     let start;
 
-    beforeEach(function() {
-      start = new Iteration({'number': 1});
+    beforeEach(function () {
+      start = new Iteration({ number: 1 });
     });
 
-    it("should create a range of iterations", function() {
-      var end = new Iteration({'number': 5});
+    it('should create a range of iterations', function () {
+      var end = new Iteration({ number: 5 });
       var iterations = Iteration.createMissingIterations('#done', start, end);
       expect(iterations.length).toEqual(3);
     });
 
-    it("should return an empty array when there is no gap between start and end", function() {
-      var end   = new Iteration({'number': 2});
+    it('should return an empty array when there is no gap between start and end', function () {
+      var end = new Iteration({ number: 2 });
       var iterations = Iteration.createMissingIterations('#done', start, end);
       expect(iterations.length).toEqual(0);
     });
 
-    it("should raise an exception if end number is less than or equal to start", function() {
-      var end   = new Iteration({'number': 1});
+    it('should raise an exception if end number is less than or equal to start', function () {
+      var end = new Iteration({ number: 1 });
       var that = start;
-      expect(function() {
+      expect(function () {
         Iteration.createMissingIterations('#done', that, end);
-      }).toThrow("end iteration number:1 must be greater than start iteration number:2");
+      }).toThrow(
+        'end iteration number:1 must be greater than start iteration number:2'
+      );
     });
 
-    it("should return an empty array when start is undefined and end is number 1", function() {
-      var end   = new Iteration({'number': 1});
-      var iterations = Iteration.createMissingIterations('#done', undefined, end);
+    it('should return an empty array when start is undefined and end is number 1', function () {
+      var end = new Iteration({ number: 1 });
+      var iterations = Iteration.createMissingIterations(
+        '#done',
+        undefined,
+        end
+      );
       expect(iterations.length).toEqual(0);
     });
 
-    it("should return a range of iterations when start is undefined", function() {
-      var end   = new Iteration({'number': 5});
-      var iterations = Iteration.createMissingIterations('#done', undefined, end);
+    it('should return a range of iterations when start is undefined', function () {
+      var end = new Iteration({ number: 5 });
+      var iterations = Iteration.createMissingIterations(
+        '#done',
+        undefined,
+        end
+      );
       expect(iterations.length).toEqual(4);
       expect(_.first(iterations).get('number')).toEqual(1);
     });
 
-    it("should return an empty array when start is undefined and end is number 1", function() {
-      var end   = new Iteration({'number': 1});
-      var iterations = Iteration.createMissingIterations('#done', undefined, end);
+    it('should return an empty array when start is undefined and end is number 1', function () {
+      var end = new Iteration({ number: 1 });
+      var iterations = Iteration.createMissingIterations(
+        '#done',
+        undefined,
+        end
+      );
       expect(iterations.length).toEqual(0);
     });
 
-    it("should return a range of iterations when start is undefined", function() {
-      var end   = new Iteration({'number': 5});
-      var iterations = Iteration.createMissingIterations('#done', undefined, end);
+    it('should return a range of iterations when start is undefined', function () {
+      var end = new Iteration({ number: 5 });
+      var iterations = Iteration.createMissingIterations(
+        '#done',
+        undefined,
+        end
+      );
       expect(iterations.length).toEqual(4);
       expect(_.first(iterations).get('number')).toEqual(1);
     });
-
   });
 
-  describe("startDate", function() {
-
-    it("should return the start date", function() {
+  describe('startDate', function () {
+    it('should return the start date', function () {
       var startDate = new Date('2011/09/26');
-      var stub = sinon.stub();
-      stub.returns(startDate);
+      var stub = vi.fn();
+      stub.mockReturnValueOnce(startDate);
       iteration.project = { getDateForIterationNumber: stub };
 
       expect(iteration.startDate()).toEqual(startDate);
       expect(stub).toHaveBeenCalledWith(iteration.get('number'));
     });
-
   });
 
-  describe("stories", function() {
+  describe('stories', function () {
     let acceptedStory;
     let finishedStory;
     let unstartedStory;
 
-    beforeEach(function() {
-      var Story = Backbone.Model.extend({name: 'story'});
-      acceptedStory = new Story({state: 'accepted'});
-      finishedStory = new Story({state: 'finished'});
-      unstartedStory = new Story({state: 'unstarted'});
-      var stories = [
-        unstartedStory, acceptedStory, finishedStory
-      ];
-      iteration.set({stories: stories});
+    beforeEach(function () {
+      var Story = Backbone.Model.extend({ name: 'story' });
+      acceptedStory = new Story({ state: 'accepted' });
+      finishedStory = new Story({ state: 'finished' });
+      unstartedStory = new Story({ state: 'unstarted' });
+      var stories = [unstartedStory, acceptedStory, finishedStory];
+      iteration.set({ stories: stories });
     });
 
-    it("returns the stories in the order they were added by default", function() {
+    it('returns the stories in the order they were added by default', function () {
       expect(iteration.stories()).toEqual([
-        unstartedStory, acceptedStory, finishedStory
+        unstartedStory,
+        acceptedStory,
+        finishedStory,
       ]);
     });
 
-    it("returns the stories accepted first if in progress iteration", function() {
-      iteration.set({column:'#in_progress'});
+    it('returns the stories accepted first if in progress iteration', function () {
+      iteration.set({ column: '#in_progress' });
       var stories = iteration.stories();
       expect(stories.length).toEqual(3);
       expect(stories[0]).toEqual(acceptedStory);
     });
 
-    it("returns stories with a given state", function() {
+    it('returns stories with a given state', function () {
       expect(iteration.storiesWithState('accepted')).toEqual([acceptedStory]);
     });
 
-    it("returns stories except a given state", function() {
-      expect(iteration.storiesExceptState('accepted')).toEqual([unstartedStory, finishedStory]);
+    it('returns stories except a given state', function () {
+      expect(iteration.storiesExceptState('accepted')).toEqual([
+        unstartedStory,
+        finishedStory,
+      ]);
     });
   });
-
 });
