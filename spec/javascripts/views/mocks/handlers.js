eb25ec2ada3448177c65a7f9113865dc09710110
William Spada <williamrspada02@gmail.com>
2024-09-10 16:18:16 -0300

    fix: http status handlers msw

--- spec/javascripts/views/mocks/handlers.js
+++ spec/javascripts/views/mocks/handlers.js
@@ -3,49 +3,44 @@ import { http, HttpResponse } from 'msw';
 const url = '/path/to/story';
 
 export const handlers = [
-  http.put(
-    url,
-    () => {
-      return HttpResponse.json({
+  http.put(url, () => {
+    return HttpResponse.json(
+      {
         story: { title: 'Story title' },
-      }).status(200);
-    },
-    { once: true }
-  ),
-  http.put(
-    url,
-    () => {
-      return HttpResponse.json({
+      },
+      { status: 200 }
+    );
+  }),
+  http.put(url, () => {
+    return HttpResponse.json(
+      {
         story: { errors: { title: ['cannot be blank'] } },
-      }).status(422);
-    },
-    { once: true }
-  ),
-  http.put(
-    url,
-    () => {
-      return HttpResponse.json({
+      },
+      { status: 422 }
+    );
+  }),
+  http.put(url, () => {
+    return HttpResponse.json(
+      {
         story: { title: 'Story title' },
-      }).status(200);
-    },
-    { once: true }
-  ),
-  http.put(
-    url,
-    () => {
-      return HttpResponse.json({
+      },
+      { status: 200 }
+    );
+  }),
+  http.put(url, () => {
+    return HttpResponse.json(
+      {
         story: { state: 'started' },
-      }).status(200);
-    },
-    { once: true }
-  ),
-  http.put(
-    url,
-    () => {
-      return HttpResponse.json({
+      },
+      { status: 200 }
+    );
+  }),
+  http.put(url, () => {
+    return HttpResponse.json(
+      {
         story: { estimate: '1' },
-      }).status(200);
-    },
-    { once: true }
-  ),
+      },
+      { status: 200 }
+    );
+  }),
 ];
