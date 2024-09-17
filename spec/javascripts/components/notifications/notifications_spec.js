ab7730018dd5e5f2f32de8bda8e4fb8d7e6822ba
William Spada <williamrspada02@gmail.com>
2024-09-17 11:09:21 -0300

    test: migrate file notifications_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/notifications/notifications_spec.js
+++ spec/javascripts/components/notifications/notifications_spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { getAllByText, render } from '@testing-library/react';
 import Notifications from 'components/Notifications';
 
 describe('<Notifications />', () => {
@@ -10,15 +10,15 @@ describe('<Notifications />', () => {
 
   it('renders component with the right number of messages', () => {
     const notifications = [
-      { id: 1, message: 'message1', type: 'success' },
-      { id: 2, message: 'message2', type: 'error' },
-      { id: 3, message: 'message3' },
+      { id: 1, message: 'message', type: 'success' },
+      { id: 2, message: 'message', type: 'error' },
+      { id: 3, message: 'message' },
     ];
 
-    const wrapper = shallow(
+    const { getAllByText } = render(
       <Notifications {...defaultProps()} notifications={notifications} />
     );
 
-    expect(wrapper.find('Message').length).toBe(notifications.length);
+    expect(getAllByText('message').length).toBe(notifications.length);
   });
 });
