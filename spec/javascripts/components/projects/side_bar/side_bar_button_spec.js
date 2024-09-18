b992d416cdb8985bd223e38fdb515cbcbc235963
William Spada <williamrspada02@gmail.com>
2024-09-18 15:26:25 -0300

    refactor: remove unnecessary waitFor import where it's unused

--- spec/javascripts/components/projects/side_bar/side_bar_button_spec.js
+++ spec/javascripts/components/projects/side_bar/side_bar_button_spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import SideBarButton from 'components/projects/SideBar/SideBarButton';
 
 describe('<SideBarButton />', () => {
