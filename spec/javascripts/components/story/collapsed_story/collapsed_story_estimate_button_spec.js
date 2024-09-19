68ddc29c8236783d28b335010f45dc2ac1566c4f
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-19 11:07:45 -0300

    test: migrate file collapsed_story_estimate_button_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/story/collapsed_story/collapsed_story_estimate_button_spec.js
+++ spec/javascripts/components/story/collapsed_story/collapsed_story_estimate_button_spec.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { CollapsedStoryEstimateButton } from 'components/story/CollapsedStory/CollapsedStoryEstimateButton';
+import React from 'react';
 
 describe('<CollapsedStoryEstimateButton />', () => {
   it("renders component with 'Fibonacci' point scale", () => {
-    const project = { pointValues: [1,2,3,5,8] }
-    const wrapper = shallow(<CollapsedStoryEstimateButton project={project}/>);
-    const text = wrapper.text()
+    const project = { pointValues: [1, 2, 3, 5, 8] };
+    render(<CollapsedStoryEstimateButton project={project} />);
 
-    project.pointValues.forEach((value)=>{
-      expect(text).toContain(value)
+    project.pointValues.forEach(value => {
+      expect(screen.getByText(value.toString())).toBeInTheDocument();
     });
   });
 
   it("renders component with 'Powers of two' point scale", () => {
-    const project = { pointValues: [1,2,4,8] }
-    const wrapper = shallow(<CollapsedStoryEstimateButton project={project}/>);
-    const text = wrapper.text()
+    const project = { pointValues: [1, 2, 4, 8] };
+    render(<CollapsedStoryEstimateButton project={project} />);
 
-    project.pointValues.forEach((value)=>{
-      expect(text).toContain(value)
+    project.pointValues.forEach(value => {
+      expect(screen.getByText(value.toString())).toBeInTheDocument();
     });
   });
 
   it("renders component with 'Linear' point scale", () => {
-    const project = { pointValues: [1,2,3,4,5] }
-    const wrapper = shallow(<CollapsedStoryEstimateButton project={project}/>);
-    const text = wrapper.text()
+    const project = { pointValues: [1, 2, 3, 4, 5] };
+    render(<CollapsedStoryEstimateButton project={project} />);
 
-    project.pointValues.forEach((value)=>{
-      expect(text).toContain(value)
+    project.pointValues.forEach(value => {
+      expect(screen.getByText(value.toString())).toBeInTheDocument();
     });
   });
 });
