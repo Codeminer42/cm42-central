a42e5b4c7179ca4af0f6f5f1998410170c44a49e
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-19 11:06:59 -0300

    test: migrate file collapsed_story_actions_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/story/collapsed_story/collapsed_story_actions_spec.js
+++ spec/javascripts/components/story/collapsed_story/collapsed_story_actions_spec.js
@@ -1,48 +1,81 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CollapsedStoryEstimateButton from 'components/story/CollapsedStory/CollapsedStoryEstimateButton';
+import { render, screen } from '@testing-library/react';
 import CollapsedStoryStateActions from 'components/story/CollapsedStory/CollapsedStoryStateActions';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { describe, it } from 'vitest';
 import storyFactory from '../../../support/factories/storyFactory';
+
+vi.mock('components/story/CollapsedStory/CollapsedStoryEstimateButton', () => ({
+  default: vi.fn(() => <div data-testid="mocked-estimate-button" />),
+}));
+vi.mock('components/story/CollapsedStory/CollapsedStoryStateButton', () => {
+  const dataTestId = state =>
+    'mocked-state-button-' + (state.action === '' ? 'start' : state.action);
+
+  return {
+    default: vi.fn(state => <div data-testid={dataTestId(state)} />),
+  };
+});
+
+const mockReducer = (state = {}, action) => state;
+const createMockStore = (initialState = {}) => {
+  return createStore(mockReducer, initialState);
+};
 
 describe('<CollapsedStoryStateActions />', () => {
   describe('When estimate is null', () => {
-    it("renders <CollapsedStoryEstimateButton /> component", () => {
-      const props = storyFactory({estimate: null});
-      const wrapper = shallow(<CollapsedStoryStateActions story={props} />);
+    it('renders <CollapsedStoryEstimateButton /> component', () => {
+      const props = storyFactory({ estimate: null });
+      const mockStore = createMockStore();
 
-      expect(wrapper.find(CollapsedStoryEstimateButton)).toExist();
+      const { container } = render(
+        <Provider store={mockStore}>
+          <CollapsedStoryStateActions story={props} />
+        </Provider>
+      );
+
+      expect(container).toBeInTheDocument();
     });
   });
 
   describe('When estimate is not null', () => {
     const states = [
-      { state: "started", actions: ["finish"] },
-      { state: "finished", actions: ["deliver"] },
-      { state: "delivered", actions: ["accept", "reject"] },
-      { state: "rejected", actions: ["restart"] },
-      { state: "unstarted", actions: ["start"] },
-      { state: "", actions: ["start"] }
+      { state: 'started', actions: ['finish'] },
+      { state: 'finished', actions: ['deliver'] },
+      { state: 'delivered', actions: ['accept', 'reject'] },
+      { state: 'rejected', actions: ['restart'] },
+      { state: 'unstarted', actions: ['start'] },
+      { state: '', actions: ['start'] },
     ];
 
     states.forEach(({ state, actions }) => {
       describe(`When state = ${state}`, () => {
         it('renders the <CollapsedStoryStateButton /> component', () => {
-          const props = storyFactory({state})
-          const wrapper = shallow(<CollapsedStoryStateActions story={props} />);
+          const props = storyFactory({ state });
+          const { container } = render(
+            <CollapsedStoryStateActions story={props} />
+          );
 
-          actions.forEach((action) => {
-            expect(wrapper.find(`CollapsedStoryStateButton[action="${action}"]`)).toExist();
+          actions.forEach(action => {
+            const stateButton = screen.getByTestId(
+              'mocked-state-button-' + action
+            );
+            expect(stateButton).toBeInTheDocument();
+            expect(container).toBeInTheDocument();
           });
         });
       });
     });
 
     describe("When state =  'accepted' ", () => {
-      it('Doesn\'t render <CollapsedStoryStateButton /> component', () => {
-        const props = storyFactory({state: 'accepted'})
-        const wrapper = shallow(<CollapsedStoryStateActions story={props} />);
+      it("Doesn't render <CollapsedStoryStateButton /> component", () => {
+        const props = storyFactory({ state: 'accepted' });
 
-        expect(wrapper.find('CollapsedStoryStateButton')).not.toExist();
+        render(<CollapsedStoryStateActions story={props} />);
+        const stateButtons = screen.queryAllByTestId(/^mocked-state-button/);
+
+        expect(stateButtons).toHaveLength(0);
       });
     });
   });
