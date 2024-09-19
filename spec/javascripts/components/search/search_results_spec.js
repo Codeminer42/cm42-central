54a68213049bd8bfd3e62e9c8c3c6e5d2be092de
William Spada <williamrspada02@gmail.com>
2024-09-19 15:15:17 -0300

    test: add initial values redux store

--- spec/javascripts/components/search/search_results_spec.js
+++ spec/javascripts/components/search/search_results_spec.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import store from 'store';
 import SearchResults from 'components/search/SearchResults';
 import { renderWithProviders } from '../setupRedux';
 import { DragDropContext } from 'react-beautiful-dnd';
 
+const emptyStory = {
+  title: '',
+  description: null,
+  estimate: '',
+  storyType: 'feature',
+  state: 'unscheduled',
+  acceptedAt: null,
+  requestedById: null,
+  ownedById: null,
+  projectId: null,
+  createdAt: '',
+  updatedAt: '',
+  position: '',
+  newPosition: null,
+  labels: [],
+  requestedByName: '',
+  ownedByName: null,
+  ownedByInitials: null,
+  releaseDate: null,
+  deliveredAt: null,
+  errors: {},
+  notes: [],
+  tasks: [],
+};
+
 describe('<SearchResults />', () => {
-  const renderComponent = () => {
+  const renderComponent = state => {
     return renderWithProviders(
       <DragDropContext onDragEnd={vi.fn()} onDragUpdate={vi.fn()}>
         <SearchResults />
       </DragDropContext>,
       {
-        store,
+        preloadedState: state,
       }
     );
   };
@@ -25,8 +49,23 @@ describe('<SearchResults />', () => {
   });
 
   describe('when isEnabled is true', () => {
-    it('renders the component', () => {
-      const { getByTestId } = renderComponent();
+    it.only('renders the component', () => {
+      const { getByTestId, store } = renderComponent({
+        stories: {
+          all: {
+            stories: [emptyStory, emptyStory, emptyStory],
+          },
+          epic: {
+            stories: [emptyStory, emptyStory, emptyStory],
+          },
+          search: {
+            stories: [emptyStory, emptyStory, emptyStory],
+          },
+        },
+      });
+
+      const state = store.getState();
+      console.log(state);
 
       expect(getByTestId('sprint-container')).toBeInTheDocument();
     });
