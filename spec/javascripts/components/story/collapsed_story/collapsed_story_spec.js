580ad2dce9ffb880a1933f4a635522ca752a7dee
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-19 12:17:59 -0300

    test: migrate file collapsed_story_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/story/collapsed_story/collapsed_story_spec.js
+++ spec/javascripts/components/story/collapsed_story/collapsed_story_spec.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { Container as CollapsedStory } from 'components/story/CollapsedStory/index';
+import React from 'react';
 import storyFactory from '../../../support/factories/storyFactory';
+
+/**
+ * needed because testing library's render method
+ * returns something html-react-parser doesn't understand
+ * and it breaks the test after the component is rendered
+ */
+vi.mock('html-react-parser', () => ({
+  default: vi.fn(),
+}));
+
+/**
+ * only direct children are mocked and tested for rendering
+ */
+vi.mock('components/story/StoryPopover', () => ({
+  default: () => <div data-testid="story-popover" />,
+}));
+vi.mock('components/story/CollapsedStory/CollapsedStoryInfo', () => ({
+  default: () => <div data-testid="collapsed-story-info" />,
+}));
+vi.mock('components/story/CollapsedStory/CollapsedStoryStateActions', () => ({
+  default: () => <div data-testid="collapsed-story-state-actions" />,
+}));
 
 describe('<CollapsedStory />', () => {
   const defaultProps = () => ({
@@ -29,46 +51,53 @@ describe('<CollapsedStory />', () => {
   describe("when estimate isn't null", () => {
     it('renders the component with Story--estimated className', () => {
       const story = storyFactory({ storyType: 'feature', estimate: 1 });
-      const wrapper = shallow(
+
+      const { container } = render(
         <CollapsedStory {...defaultProps()} story={story} />
       );
-      expect(wrapper).toHaveClassName('Story--estimated');
+
+      expect(container.firstChild).toHaveClass('Story--estimated');
     });
   });
 
   describe('when estimate is null', () => {
     it('renders the component with Story--unestimated className', () => {
       const story = storyFactory({ storyType: 'feature', estimate: null });
-      const wrapper = shallow(
+
+      const { container } = render(
         <CollapsedStory {...defaultProps()} story={story} />
       );
 
-      expect(wrapper).toHaveClassName('Story--unestimated');
+      expect(container.firstChild).toHaveClass('Story--unestimated');
     });
   });
 
   describe('when storyType = release', () => {
     it('renders the component with Story--release className', () => {
       const story = storyFactory({ storyType: 'release' });
-      const wrapper = shallow(
+
+      const { container } = render(
         <CollapsedStory {...defaultProps()} story={story} />
       );
 
-      expect(wrapper).toHaveClassName('Story--release');
+      expect(container.firstChild).toHaveClass('Story--release');
     });
   });
 
   it('renders children components', () => {
     const story = storyFactory({ storyType: 'feature', estimate: 1 });
-    const wrapper = shallow(
-      <CollapsedStory {...defaultProps()} story={story} />
-    );
+    const components = [
+      'story-popover',
+      'collapsed-story-info',
+      'collapsed-story-state-actions',
+    ];
 
-    expect(wrapper.find('StoryPopover')).toExist();
-    expect(wrapper.find('CollapsedStoryIcon')).toExist();
-    expect(wrapper.find('CollapsedStoryEstimate')).toExist();
-    expect(wrapper.find('StoryDescriptionIcon')).toExist();
-    expect(wrapper.find('CollapsedStoryInfo')).toExist();
-    expect(wrapper.find('CollapsedStoryStateActions')).toExist();
+    render(<CollapsedStory {...defaultProps()} story={story} />);
+
+    for (const testId of components) {
+      const child = screen.getByTestId(testId);
+
+      expect(child).toBeInTheDocument();
+    }
   });
 });
