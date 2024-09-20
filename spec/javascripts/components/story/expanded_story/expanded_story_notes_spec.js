ce32cce7108d58ca8467d438bba9c3e6fe5ccc61
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-20 16:57:28 -0300

    test: migrate file expanded_story_notes_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/story/expanded_story/expanded_story_notes_spec.js
+++ spec/javascripts/components/story/expanded_story/expanded_story_notes_spec.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { fireEvent } from '@testing-library/react';
 import ExpandedStoryNotes from 'components/story/ExpandedStory/ExpandedStoryNotes';
+import React from 'react';
 import storyFactory from '../../../support/factories/storyFactory';
+import { renderWithProviders } from '../../setupRedux';
 
 describe('<ExpandedStoryNotes />', () => {
-  let onCreate;
-  let onDelete;
+  vi.spyOn(window.md, 'makeHtml').mockReturnValue('<p>Test</p>');
+
+  const onCreate = vi.fn();
+  const onDelete = vi.fn();
 
   const newStory = notes => ({
     ...storyFactory({ notes: notes || [] }),
@@ -14,91 +17,94 @@ describe('<ExpandedStoryNotes />', () => {
     },
   });
 
-  beforeEach(() => {
-    onCreate = vi.fn();
-    onDelete = vi.fn();
-  });
+  const renderComponent = props => {
+    const defaultProps = {
+      story: {
+        ...storyFactory({ notes: [] }),
+        _editing: {
+          ...storyFactory({ notes: [] }),
+        },
+      },
+      disabled: false,
+    };
+
+    const mergedProps = { ...defaultProps, ...props };
+
+    return renderWithProviders(<ExpandedStoryNotes {...mergedProps} />);
+  };
 
   it('renders component title', () => {
-    const story = newStory();
+    const props = {
+      onCreate,
+      onDelete,
+    };
 
-    const wrapper = mount(
-      <ExpandedStoryNotes
-        story={story}
-        onCreate={onCreate}
-        onDelete={onDelete}
-        disabled={false}
-      />
-    );
+    const { container } = renderComponent(props);
+    const title = container.querySelector('.Story__section-title');
 
-    expect(wrapper.find('.Story__section-title').text()).toContain(
-      I18n.t('story.notes')
-    );
+    expect(title).toHaveTextContent(I18n.t('story.notes'));
   });
 
   it('renders component content', () => {
-    const story = newStory();
+    const props = {
+      onCreate,
+      onDelete,
+      story: {
+        ...storyFactory({ notes: [{ username: 'foo' }] }),
+        _editing: {
+          ...storyFactory({ notes: [{ username: 'foo' }] }),
+        },
+      },
+    };
 
-    const wrapper = mount(
-      <ExpandedStoryNotes
-        story={story}
-        onCreate={onCreate}
-        onDelete={onDelete}
-        disabled={false}
-      />
-    );
+    const { container } = renderComponent(props);
+    const createNoteText = container.querySelector('.create-note-text');
+    const createNoteButton = container.querySelector('.create-note-button');
+    const storySectionNotes = container.querySelector('.Story__section__notes');
+    const notelist = container.querySelector('.markdown-wrapper__text-right');
 
-    expect(wrapper.find('.create-note-text')).toExist();
-    expect(wrapper.find('.create-note-button')).toExist();
-    expect(wrapper.find('.Story__section__notes')).toExist();
-    expect(wrapper.find('NotesList')).toExist();
+    expect(createNoteText).toBeInTheDocument();
+    expect(createNoteButton).toBeInTheDocument();
+    expect(storySectionNotes).toBeInTheDocument();
+    expect(notelist).toBeInTheDocument();
   });
 
   describe('when component is enabled', () => {
     describe('when user create a new note', () => {
       it('triggers the onCreate callback passing the note', () => {
-        const story = newStory();
-
-        const change = 'newNote';
-
         const onCreateSpy = vi.fn();
+        const change = 'newNote';
+        const props = {
+          onCreate: onCreateSpy,
+          onDelete,
+          disabled: false,
+        };
 
-        const wrapper = mount(
-          <ExpandedStoryNotes
-            story={story}
-            onCreate={onCreateSpy}
-            onDelete={onDelete}
-            disabled={false}
-          />
-        );
+        const { container } = renderComponent(props);
+        const textArea = container.querySelector('.create-note-text');
+        const button = container.querySelector('.create-note-button input');
 
-        const textArea = wrapper.find('.create-note-text');
-        const button = wrapper.find('.create-note-button input');
-
-        textArea.simulate('change', { target: { value: change } });
-        button.simulate('click');
+        fireEvent.change(textArea, { target: { value: change } });
+        fireEvent.click(button);
 
         expect(onCreateSpy).toHaveBeenCalledWith(change);
+        expect(onCreateSpy).toHaveBeenCalledTimes(1);
       });
     });
 
     it('disables the add note button if text area is empty', () => {
-      const story = newStory();
+      const props = {
+        onCreate,
+        onDelete,
+        disabled: false,
+      };
 
-      const wrapper = shallow(
-        <ExpandedStoryNotes
-          story={story}
-          onCreate={onCreate}
-          onDelete={onDelete}
-          disabled={false}
-        />
-      );
+      const { container } = renderComponent(props);
+      const textArea = container.querySelector('.create-note-text');
+      const button = container.querySelector('.create-note-button input');
+      fireEvent.change(textArea, { target: { value: '' } });
 
-      const textArea = wrapper.find('.create-note-text');
-      const button = wrapper.find('.create-note-button input');
-
-      textArea.simulate('change', { target: { value: '' } });
-      expect(button.prop('disabled')).toBe(true);
+      expect(button).toBeDisabled();
     });
   });
 
@@ -106,49 +112,47 @@ describe('<ExpandedStoryNotes />', () => {
     it('does not render a create note button', () => {
       const notes = [{ id: 0, note: 'foo' }];
       const story = newStory(notes);
+      const props = {
+        onCreate,
+        onDelete,
+        disabled: true,
+        story,
+      };
 
-      const wrapper = shallow(
-        <ExpandedStoryNotes
-          story={story}
-          onCreate={onCreate}
-          onDelete={onDelete}
-          disabled={true}
-        />
-      );
+      const { container } = renderComponent(props);
+      const createNoteButton = container.querySelector('.create-note-button');
 
-      expect(wrapper.exists('.create-note-button')).toBe(false);
+      expect(createNoteButton).toBeNull();
     });
 
     it('does not render a create note text area', () => {
       const notes = [{ id: 0, note: 'foo' }];
       const story = newStory(notes);
+      const props = {
+        onCreate,
+        onDelete,
+        disabled: true,
+        story,
+      };
 
-      const wrapper = shallow(
-        <ExpandedStoryNotes
-          story={story}
-          onCreate={onCreate}
-          onDelete={onDelete}
-          disabled={true}
-        />
-      );
+      const { container } = renderComponent(props);
 
-      expect(wrapper.exists('.create-note-text')).toBe(false);
+      expect(container.querySelector('.create-note-text')).toBeNull();
     });
 
     describe('when there are no notes', () => {
       it('renders nothing', () => {
         const story = newStory();
+        const props = {
+          onCreate,
+          onDelete,
+          disabled: true,
+          story,
+        };
 
-        const wrapper = shallow(
-          <ExpandedStoryNotes
-            story={story}
-            onCreate={onCreate}
-            onDelete={onDelete}
-            disabled={true}
-          />
-        );
+        const { container } = renderComponent(props);
 
-        expect(wrapper.html()).toBeNull();
+        expect(container.innerHTML).toBe('');
       });
     });
   });
