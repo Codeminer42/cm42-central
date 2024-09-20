be32eb290dfb632d03ad8a55b75fe911ce75124f
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-20 12:08:12 -0300

    test: migrate file expanded_expanded_story_description_content_area_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/story/expanded_story/expanded_story_description/expanded_story_description_content_area_spec.js
+++ spec/javascripts/components/story/expanded_story/expanded_story_description/expanded_story_description_content_area_spec.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import ExpandedStoryContentArea from 'components/story/ExpandedStory/ExpandedStoryDescription/ExpandedStoryContentArea';
+import React from 'react';
+import { renderWithProviders } from '../../../setupRedux';
 
 describe('<ExpandedStoryContentArea />', () => {
-  const renderContent = propsOverride => {
-    const wrapper = shallow(
-      <ExpandedStoryContentArea
-        onClick={vi.fn()}
-        description=""
-        {...propsOverride}
-      />
-    );
-    const descriptionContent = wrapper.find('[data-id="description-content"]');
-    const editButton = wrapper.find('[data-id="edit-button"]');
+  const markdownSpy = vi
+    .spyOn(window.md, 'makeHtml')
+    .mockReturnValue('<p>Test</p>');
 
-    return { wrapper, descriptionContent, editButton };
+  const renderComponent = props => {
+    const defaultProps = {
+      onClick: vi.fn(),
+      description: '',
+    };
+
+    const mergedProps = { ...defaultProps, ...props };
+
+    return renderWithProviders(<ExpandedStoryContentArea {...mergedProps} />);
   };
 
   describe("when description isn't null", () => {
     const description = 'description example';
 
     it('renders <ExpandedStoryDescriptionContent />', () => {
-      const { descriptionContent } = renderContent({ description });
-      expect(descriptionContent.exists()).toBeTruthy();
+      renderComponent({ description });
+
+      expect(markdownSpy).toHaveBeenCalledWith(description);
     });
   });
 });
