84cceb91fb28f3aaeeb231253d1a524b07e2c6fb
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-20 14:00:40 -0300

    test: migrate file expanded_story_release_date_spec.js from enzyme to react-testing-library

--- spec/javascripts/components/story/expanded_story/expanded_story_release/expanded_story_release_date_spec.js
+++ spec/javascripts/components/story/expanded_story/expanded_story_release/expanded_story_release_date_spec.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { mount } from 'enzyme';
+import { screen } from '@testing-library/react';
 import ExpandedStoryReleaseDate from 'components/story/ExpandedStory/ExpandedStoryRelease/ExpandedStoryReleaseDate';
+import React from 'react';
 import storyFactory from '../../../../support/factories/storyFactory';
+import { renderWithProviders } from '../../../setupRedux';
 
 describe('<ExpandedStoryReleaseDate />', () => {
-  const defaultProps = () => ({
-    story: {
-      ...storyFactory(),
-      _editing: storyFactory({ releaseDate: null }),
-    },
-    disabled: false,
-    onEdit: vi.fn(),
-  });
+  const renderComponent = props => {
+    const defaultProps = {
+      story: {
+        ...storyFactory(),
+        _editing: storyFactory({ releaseDate: null }),
+      },
+      disabled: false,
+      onEdit: vi.fn(),
+    };
+    const mergedProps = { ...defaultProps, ...props };
+
+    return renderWithProviders(<ExpandedStoryReleaseDate {...mergedProps} />);
+  };
 
   it('renders component title', () => {
-    const wrapper = mount(<ExpandedStoryReleaseDate {...defaultProps()} />);
+    renderComponent();
 
-    expect(wrapper.text()).toContain(
+    const releaseText = screen.getByText(
       I18n.t('activerecord.attributes.story.release_date')
     );
+    expect(releaseText).toBeInTheDocument();
   });
 });
