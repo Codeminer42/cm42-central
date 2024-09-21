fd1c454db0ef3ea031266cf680dbc731003cfa51
Mike Valeriano <mike.valeriano@codeminer42.com>
2024-09-21 04:44:07 -0300

    test: migrate file expanded_story_tooltip_spec.jsx from enzyme to react-testing-library

--- spec/javascripts/components/story/expanded_story/expanded_story_tooltip_spec.js
+++ spec/javascripts/components/story/expanded_story/expanded_story_tooltip_spec.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import ExpandedStoryTooltip from 'components/story/ExpandedStory/ExpandedStoryToolTip';
+import React from 'react';
+import { renderWithProviders } from '../../setupRedux';
 
 describe('<ExpandedStoryTooltip />', () => {
   it('renders <Popover />', () => {
-    const wrapper = shallow(<ExpandedStoryTooltip text="any text" />);
-    expect(wrapper.find('Popover').exists()).toBe(true);
+    const { container } = renderWithProviders(<ExpandedStoryTooltip />);
+
+    const tooltip = container.querySelector('.infoToolTip');
+
+    expect(tooltip).toBeInTheDocument();
   });
 });
