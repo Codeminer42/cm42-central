fccb519e541e2d1170ae9cd5af92ae96a6cdfd92
Daniel M Brasil <danielmbrasil@protonmail.com>
2024-10-01 22:39:29 -0300

    chore: remove unused Team#allowed_domain? (#964)

--- app/models/team.rb
+++ app/models/team.rb
@@ -23,16 +23,6 @@ class Team < ApplicationRecord
 
   has_attachment :logo, accept: %i[jpg png gif bmp]
 
-  def allowed_domain?(email)
-    whitelist = (registration_domain_whitelist || '').split(DOMAIN_SEPARATORS_REGEX).map(&:strip)
-    blacklist = (registration_domain_blacklist || '').split(DOMAIN_SEPARATORS_REGEX).map(&:strip)
-    has_whitelist = true
-    has_whitelist = whitelist.any? { |domain| email.include?(domain) } unless whitelist.empty?
-    has_blacklist = false
-    has_blacklist = blacklist.any? { |domain| email.include?(domain) } unless blacklist.empty?
-    has_whitelist && !has_blacklist
-  end
-
   def to_param
     ::FriendlyId::Disabler.disabled? ? (id&.to_s) : super
   end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,31 +9,6 @@ describe Team, type: :model do
   it { is_expected.to have_many(:api_tokens) }
   it { is_expected.to have_many(:tag_groups) }
 
-  context '#allowed_domain' do
-    it 'is in whitelist' do
-      subject.registration_domain_whitelist = 'codeminer42.com, uol.com.br'
-      expect(subject.allowed_domain?('foo@codeminer42.com')).to be_truthy
-      expect(subject.allowed_domain?('foo@uol.com.br')).to be_truthy
-      expect(subject.allowed_domain?('foo@yahoo.com.br')).to be_falsey
-    end
-
-    it 'is not in blacklist' do
-      subject.registration_domain_blacklist = "hotmail.com\ngmail.com\nyahoo.com.br"
-      expect(subject.allowed_domain?('foo@codeminer42.com')).to be_truthy
-      expect(subject.allowed_domain?('foo@uol.com.br')).to be_truthy
-      expect(subject.allowed_domain?('foo@yahoo.com.br')).to be_falsey
-    end
-
-    it 'it is both in the whitelist and not in the blacklist' do
-      subject.registration_domain_whitelist = 'codeminer42.com, uol.com.br'
-      subject.registration_domain_blacklist = "hotmail.com\ngmail.com\nyahoo.com.br"
-      expect(subject.allowed_domain?('foo@codeminer42.com')).to be_truthy
-      expect(subject.allowed_domain?('foo@uol.com.br')).to be_truthy
-      expect(subject.allowed_domain?('foo@yahoo.com.br')).to be_falsey
-      expect(subject.allowed_domain?('foo@gmail.com.br')).to be_falsey
-    end
-  end
-
   describe 'scopes' do
     describe '.ordered_by_name' do
       let!(:first_team) { create(:team, name: 'Team 2') }
