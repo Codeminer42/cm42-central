0a93401d235097b1843427390b9c60507f6197e0
William Spada <90157791+wrspada02@users.noreply.github.com>
2024-10-28 09:58:40 -0300

    Remove react-clipboard.js and replace by using react-copy-to-clipboard package (#979)
    * build: remove react-clipboard.js and replace by using react-copy-to-clipboard package
    
    * chore: follow talysson request changes clipboard texts
    
    * test: remove repeat test to check copy clibpoard id
    
    * refactor: remove unused mock config
    
    * chore: add text #id to StoryCopyClipboard component
    
    * test: add test to check clipboard value when clicks StoryCopyIdClipboard

--- spec/javascripts/components/story/story_copy_id_clipboard_spec.jsx
+++ spec/javascripts/components/story/story_copy_id_clipboard_spec.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import StoryCopyIdClipboard from 'components/story/StoryCopyIdClipboard';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 vi.mock('react-clipboard.js', () => ({
   default: ({ children, ...props }) => <button {...props}>{children}</button>,
 }));
 
 describe('<StoryCopyIdClipboard />', function () {
-  let fn;
-
-  beforeEach(function () {
-    fn = vi.spyOn(I18n, 't');
-  });
-
-  afterEach(function () {
-    fn.mockRestore();
-  });
-
   it('should render story id text', function () {
     const component = render(<StoryCopyIdClipboard id={70} />);
     expect(component.getByText('#70')).toBeInTheDocument();
@@ -30,8 +20,17 @@ describe('<StoryCopyIdClipboard />', function () {
     ).toBeDefined();
   });
 
-  it('should render copy id title', function () {
-    render(<StoryCopyIdClipboard id={70} />);
-    expect(fn).toHaveBeenCalledWith('story.events.copy_id');
+  it('should copy correct id', async () => {
+    const onCopy = vi.fn();
+
+    const { container } = render(
+      <StoryCopyIdClipboard id={70} onCopy={onCopy} />
+    );
+
+    const paragraph = container.querySelector('p');
+
+    fireEvent.click(paragraph);
+
+    expect(onCopy).toHaveBeenCalledWith('#70', false);
   });
 });
