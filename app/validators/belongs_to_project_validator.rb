cc5bc8f49ac652d369bfdf37db1d71b48cea2312
Daniel M Brasil <danielmbrasil@protonmail.com>
2024-11-01 17:22:30 -0300

    fix codeclimate errors

--- app/validators/belongs_to_project_validator.rb
+++ app/validators/belongs_to_project_validator.rb
@@ -2,10 +2,10 @@ class BelongsToProjectValidator < ActiveModel::EachValidator
   # Checks that the parameter being validated represents a User#id that
   # is a member of the object.project
   def validate_each(record, attribute, value)
-    if record.project && !value.nil?
-      unless record.project.user_ids.include?(value)
-        record.errors.add(attribute, message: 'user is not a member of this project')
-      end
-    end
+    return if record.project.blank? || value.blank?
+
+    return if record.project.user_ids.include?(value)
+
+    record.errors.add(attribute, message: 'user is not a member of this project')
   end
 end
--- app/validators/changed_validator.rb
+++ app/validators/changed_validator.rb
@@ -1,8 +1,8 @@
 class ChangedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if record.action != 'update' 
+    return if record.action != 'update'
     return if value&.valid? && value&.saved_changes.present?
-      
+
     record.errors.add(attribute, message: (options[:message] || "Record didn't change"))
   end
 end
