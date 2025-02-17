53e1e3c472a6ae2c8a9de981152b4be3fef0a1e9
julialbq <julia.albq@gmail.com>
2025-02-17 14:30:06 -0300

    refactor: TaggedInput class to function component

--- app/assets/javascripts/components/jquery_wrappers/TaggedInput.jsx
+++ app/assets/javascripts/components/jquery_wrappers/TaggedInput.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-class TaggedInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.saveInputRef = input => {
-      this.input = input;
+const TaggedInput = ({ availableLabels, disabled, onChange, input }) => {
+  const inputRef = useRef(null);
+
+  const tagitProperties = {
+    availableTags: availableLabels,
+    readOnly: disabled,
+  };
+
+  const loadTagit = () => {
+    if (inputRef.current) {
+      $(inputRef.current).tagit(tagitProperties).on('change', onChange);
+    }
+  };
+
+  useEffect(() => {
+    loadTagit();
+
+    return () => {
+      if (inputRef.current) {
+        $(inputRef.current).tagit('destroy');
+      }
     };
-  }
+  }, [availableLabels, disabled, onChange]);
 
-  componentDidMount() {
-    this.loadTagit();
-  }
-
-  loadTagit() {
-    $(this.input)
-      .tagit(this.tagitProperties())
-      .on('change', this.props.onChange);
-  }
-
-  tagitProperties() {
-    return {
-      availableTags: this.props.availableLabels,
-      readOnly: this.props.disabled,
-    };
-  }
-
-  render() {
-    return <input ref={this.saveInputRef} {...this.props.input} />;
-  }
-}
+  return <input ref={inputRef} {...input} />;
+};
 
 export default TaggedInput;
