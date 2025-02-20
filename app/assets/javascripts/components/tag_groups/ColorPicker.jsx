6e9b1f4e0c6ba4b4266401579b2e36cb2f7239ab
julialbq <julia.albq@gmail.com>
2025-02-20 14:56:34 -0300

    refactor: ColorPick class to function component

--- app/assets/javascripts/components/tag_groups/ColorPicker.jsx
+++ app/assets/javascripts/components/tag_groups/ColorPicker.jsx
@@ -1,57 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import { TwitterPicker } from 'react-color';
 
-export default class ColorPick extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayColorPicker: false,
-      color: this.props.color,
-    };
+const ColorPick = ({ color: initialColor }) => {
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  const [color, setColor] = useState(initialColor);
 
-    this.handleClick = this.handleClick.bind(this);
+  const handleClick = () => setDisplayColorPicker(!displayColorPicker);
+  const handleClose = () => setDisplayColorPicker(false);
+  const handleChange = color => setColor(color.hex);
 
-    this.handleClose = this.handleClose.bind(this);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleClick() {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker });
-  }
-
-  handleClose() {
-    this.setState({ displayColorPicker: false });
-  }
-
-  handleChange(color) {
-    this.setState({ color: color.hex });
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <div onClick={this.handleClick} className="swatch">
-          <div
-            style={{ backgroundColor: this.state.color }}
-            className="color-box"
-          />
-          <input
-            type="hidden"
-            value={this.state.color}
-            name="tag_group[bg_color]"
-          />
+  return (
+    <>
+      <div onClick={handleClick} className="swatch">
+        <div style={{ backgroundColor: color }} className="color-box" />
+        <input type="hidden" value={color} name="tag_group[bg_color]" />
+      </div>
+      {displayColorPicker && (
+        <div className="color-box-popover">
+          <div className="color-box-cover" onClick={handleClose} />
+          <TwitterPicker color={color} onChange={handleChange} />
         </div>
-        {this.state.displayColorPicker && (
-          <div className="color-box-popover">
-            <div className="color-box-cover" onClick={this.handleClose} />
-            <TwitterPicker
-              color={this.state.color}
-              onChange={this.handleChange}
-            />
-          </div>
-        )}
-      </Fragment>
-    );
-  }
-}
+      )}
+    </>
+  );
+};
+
+export default ColorPick;
