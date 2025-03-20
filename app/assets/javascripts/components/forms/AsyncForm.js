e01fb1d9a7ad62018d0636ca113004399c528016
julialbq <julia.albq@gmail.com>
2025-03-20 14:14:53 -0300

    refactor: wrap handleSubmit in useCallback according to reviewer suggestion

--- app/assets/javascripts/components/forms/AsyncForm.js
+++ app/assets/javascripts/components/forms/AsyncForm.js
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const AsyncForm = ({ onSubmit, getFormData, children }) => {
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async ev => {
-    ev.preventDefault();
-    setLoading(true);
-    try {
-      await onSubmit(getFormData());
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-    }
-  };
+  const handleSubmit = useCallback(
+    async ev => {
+      ev.preventDefault();
+      setLoading(true);
+      try {
+        await onSubmit(getFormData());
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    },
+    [onSubmit, getFormData]
+  );
 
   return children({ loading, handleSubmit });
 };
