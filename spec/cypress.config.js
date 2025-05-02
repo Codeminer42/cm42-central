68b8e8ce888157fb8b616f5da1470efb0214b97d
Vitor Ferreira <36849033+vitorfrs-dev@users.noreply.github.com>
2025-05-02 09:44:26 -0300

    Chore: Update Cypress config file (#1002)
    * chore: update Cypress config file
    - I followed the migration guide on https://dev.to/cypress/migrating-to-cypress-10-2gbe
    - Renamed the files according to the migration guide
    - Updated the config file with the correct paths
    
    * chore: upgrade actions/checkout and actions/setup-node to v4

--- spec/cypress.config.js
+++ spec/cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'http://localhost:3000',
     fixturesFolder: 'cypress/fixtures',
-    supportFile: 'cypress/support/index.js',
-    specPattern: 'cypress/integration/**/*_spec.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
   },
