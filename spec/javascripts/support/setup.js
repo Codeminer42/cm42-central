4e50a3daaac77424d3ed8fefbc6c40a00f1c9e05
jv-vogler <jvsvogler@gmail.com>
2025-08-07 12:19:22 -0300

    chore: install user-event testing util

--- spec/javascripts/support/setup.js
+++ spec/javascripts/support/setup.js
@@ -2,10 +2,13 @@ import 'libs/index';
 import 'raf/polyfill';
 
 import * as JestDom from '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 const { toHaveStyle: toHaveDomStyle, ...jestDomRest } = JestDom;
 
 expect.extend({ toHaveDomStyle, ...jestDomRest });
+
+export const user = userEvent.setup();
 
 window.md = { makeHtml: function () {} };
 window.process.env.CLOUDINARY_URL = 'cloudinary://username:password@localhost';
